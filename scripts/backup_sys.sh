13d41cc10a36d2ea0db5c3a7ce5754f8bf34bd49
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2025-12-02 20:00:13 +0000

    Update

---
+++ scripts/backup_sys.sh
@@ -0,0 +1,17 @@
+borg create --progress "/mnt/Backups/ArchB/BorgBackups2::$(date +%Y-%m-%d_%H-%M-%S)" \ 
+--exclude '/home/archboom/Downloads/*.iso' \
+  --exclude '/home/archboom/Downloads/*.tar.gz' \
+  --exclude '/home/archboom/Downloads/tmp*' \
+  --exclude '/home/archboom/Documents/tmp*' \
+  --exclude '/home/archboom/dotfiles/.git' \
+  --exclude '/home/archboom/dotfiles/.gitignore' \
+  --exclude '/home/archboom/Documents/venv' \
+  /home/archboom/Documents \
+  /home/archboom/Downloads \
+  /home/archboom/Pictures/Screenshots \
+  /home/archboom/dotfiles \
+  "/home/archboom/.local/share/Anki2/User 1/backups"
+
+borg list /mnt/Backups/ArchB/BorgBackups2/
+
+sleep 30
--- scripts/half-backup.sh
+++
@@ -1,8 +0,0 @@
-duplicity incremental --no-encryption \
-  --include /home/archboom/Documents \
-  --include /home/archboom/Downloads \
-  --include /home/archboom/Pictures/Screenshots \
-  --include /home/archboom/dotfiles \
-  --include "/home/archboom/.local/share/Anki2/User 1/backups" \
-  --exclude '**' \
-  / "file:///mnt/Backups/Linux backups/"
