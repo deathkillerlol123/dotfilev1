acd93862ce9ea6267111d8fa9c1d407fe7fc54df
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2025-12-17 15:03:31 +0000

    Update

--- scripts/cleaner.sh
+++ scripts/cleaner.sh
@@ -3,13 +3,13 @@
 rm -rf ~/.cache/*
 mkdir ~/.cache/swww
 
-rm -rf ~/.local/share/Trash/*
+sudo rm -rf ~/.local/share/Trash/*
 
 flatpak uninstall --unused
 
 sudo rm -rf /tmp/* /var/cache/fontconfig/* /var/cache/man/*
 
-yarn cache clean
+#yarn cache clean
 cargo clean
 
 rm -rf ~/.cargo/registry
@@ -18,18 +18,14 @@ rm -rf ~/.cargo/git
 sudo pacman -Sc
 sudo pacman -Scc
 
-
 sudo journalctl --vacuum-time=7d
 
-rm -rf ~/.cache/* ~/.cargo/{registry,git} ~/.yarn/cache ~/.var/app/*/cache ~/.mozilla/firefox/*/cache2/* && \
-yarn cache clean --silent
- 
+rm -rf ~/.cache/* ~/.cargo/{registry,git} ~/.var/app/*/cache ~/.mozilla/firefox/*/cache2/*
+#yarn cache clean --silent
 
 sudo rm -rf /var/tmp/*
 sudo rm -rf /tmp/*
 
-
 sudo rm -rf /var/cache/pacman/pkg/*
-
 
 ~/dotfiles/scripts/tester.sh
