e8d560089f64eb20367d2d8bbd8055bec6c9283a
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-01-25 20:51:21 +0000

    Update

--- scripts/waybar.sh
+++
@@ -1,2 +0,0 @@
-waybar -c ~/dotfiles/.config/waybar/config_main -s ~/dotfiles/.config/waybar/style_main.css &
-waybar -c ~/dotfiles/.config/waybar/config_laptop -s ~/dotfiles/.config/waybar/style_laptop.css &
