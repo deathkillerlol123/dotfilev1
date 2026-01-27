18aeca8e82cb5f792ba8eca3dc1c888b60dc6c47
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-01-27 22:33:45 +0000

    Update

---
+++ scripts/updatesys.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -euo pipefail
+
+SHOW=false
+for arg in "$@"; do
+  case "$arg" in
+  -show)
+    SHOW=true
+    ;;
+  *) ;;
+  esac
+done
+
+if $SHOW; then
+  sudo pacman -Syu
+  yay -Syu
+else
+  sudo pacman -Syu --noconfirm
+  yay -Syu --noconfirm
+fi
+
+mkdir -p ~/.cache/swww
+rm -rf ~/.cache/* ~/.var/app/*/cache ~/.mozilla/firefox/*/cache2/* || true
+
+rm -rf ~/.local/share/Trash/* || true
+
+flatpak uninstall --unused -y
+flatpak repair || true
+
+sudo find /tmp /var/tmp -mindepth 1 -delete || true
+sudo rm -rf /var/cache/fontconfig/* /var/cache/man/*
+
+sudo pacman -Sc --noconfirm
+yay -Sc --noconfirm
+
+sudo journalctl --vacuum-time=7d
+
+~/dotfiles/scripts/tester.sh
