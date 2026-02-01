a5efc2a6f102681412cd32696d9df8d4534b2309
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-02-01 13:35:52 +0000

    Update

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -1,10 +1,17 @@
 swww-daemon &
 
-"$HOME/dotfiles/scripts/colorschem/start_wall.sh"
-"$HOME/dotfiles/scripts/disable-wifi-power.sh"
-"$HOME/dotfiles/scripts/niri_monitors/placement.sh"
-
-wayscriber --daemon &
 copyq &
 hyprpm reload
 dunst &
+
+sleep 1
+
+"$HOME/dotfiles/scripts/colorschem/start_wall.sh"
+
+sleep 1
+
+"$HOME/dotfiles/scripts/niri_monitors/placement.sh"
+
+sleep 1
+
+"$HOME/dotfiles/scripts/disable-wifi-power.sh"
