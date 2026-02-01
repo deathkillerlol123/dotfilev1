280ca346d3501bde073f0075c54122ae5efa2ed5
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-02-01 22:29:50 +0000

    Update

--- scripts/niri_monitors/placement.sh
+++ scripts/niri_monitors/placement.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set DP (left) and HDMI (right) without setting primary
-wlr-randr --output DP-1 --mode 1920x1080 --pos 0,65
-wlr-randr --output DP-2 --mode 1920x1080 --pos 0,65
+wlr-randr --output DP-1 --mode 1920x1080 --pos 0,0 --transform 90
+wlr-randr --output DP-2 --mode 1920x1080 --pos 0,0 --transform 90
 
-wlr-randr --output HDMI-A-1 --mode 1920x1080 --pos 1920,0
-wlr-randr --output eDP-1 --mode 1920x1080 --pos 1920,1080
+wlr-randr --output HDMI-A-1 --mode 1920x1080 --pos 1080,0
+wlr-randr --output eDP-1 --mode 1920x1080 --pos 1080,1080
