6e6ad58c80ec9eb8a11f690bea974a0132ffda20
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-02-04 21:52:50 +0000

    Update

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -4,14 +4,8 @@ copyq &
 hyprpm reload
 swaync &
 
-sleep 1
-
-"$HOME/dotfiles/scripts/colorschem/start_wall.sh"
-
-sleep 1
-
 "$HOME/dotfiles/scripts/niri_monitors/placement.sh"
 
-sleep 1
+"$HOME/dotfiles/scripts/colorschem/start_wall.sh"
 
 "$HOME/dotfiles/scripts/disable-wifi-power.sh"
