9f8d7e31f510be5c8aaa1261aa7c559acd21f302
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-02-28 21:21:00 +0000

    Update repository with latest changes

--- .config/waybar/random_wallpaper.sh
+++ .config/waybar/random_wallpaper.sh
@@ -8,17 +8,17 @@ LAST_WALL="$HOME/dotfiles/scripts/colorschem/last_wall.txt"
 FILE=$(find ~/Pictures/Wallpapers/ -type f -print0 | shuf -zn1 | xargs -0 echo)
 echo "$FILE"
 if [ -n "$FILE" ]; then
-	echo "$FILE" >"$LAST_WALL"
-	swww img "$FILE" --transition-type fade --transition-step 90 --transition-duration 5
-	wal -i "$FILE" -n
+  echo "$FILE" >"$LAST_WALL"
+  swww img "$FILE" --transition-type fade --transition-step 90 --transition-duration 5
+  wal -i "$FILE" -n
 
-	pkill -f rofi
-	cp ~/.cache/wal/colors-waybar.css ~/dotfiles/.config/waybar/colors1.css
-	cp ~/dotfiles/.config/waybar/colors1.css ~/.config/mozilla/firefox-devedition/chrome
-	cp colors1.css ~/dotfiles/.config/wlogout/colors1.css
-	cp ~/.cache/wal/colors-kitty.conf ~/dotfiles/.config/kitty/
+  pkill -f rofi
+  cp ~/.cache/wal/colors-waybar.css ~/dotfiles/.config/waybar/colors1.css
+  cp ~/.cache/wal/colors.css ~/dotfiles/.config/mozilla/firefox/3rv9ix8a.dev-edition-default/chrome/colors1.css
+  cp colors1.css ~/dotfiles/.config/wlogout/colors1.css
+  cp ~/.cache/wal/colors-kitty.conf ~/dotfiles/.config/kitty/
 
-	"$HOME/dotfiles/.config/waybar/css_rasi.sh"
-	mv colors1.rasi ~/dotfiles/.config/rofi/colors1.rasi
+  "$HOME/dotfiles/.config/waybar/css_rasi.sh"
+  mv colors1.rasi ~/dotfiles/.config/rofi/colors1.rasi
 
 fi
