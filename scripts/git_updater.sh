0121fa148394df2fef9919d94e8afcf8b5873694
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-02-28 20:28:46 +0000

    Update repository with latest changes

--- scripts/git_updater.sh
+++ scripts/git_updater.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-cd ~/dotfiles || exit 1
+
+cd ~/dotfiles/
+
+if [ ! -d ".git" ]; then
+  git init
+fi
+
 git add .
-git commit -m "Update"
-git push origin main
+git commit -m "Update repository with latest changes"
+git push -u origin main
+
+echo "Repository updated successfully!"
