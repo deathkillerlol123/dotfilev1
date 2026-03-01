4554ba527ad1ef625461116986f2fafc80e27fd8
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-03-01 14:41:39 +0000

    Update repository with latest changes

--- .config/waybar/random_wallpaper.sh
+++ .config/waybar/random_wallpaper.sh
@@ -11,13 +11,13 @@ if [ -n "$FILE" ]; then
   echo "$FILE" >"$LAST_WALL"
 
   swww img "$FILE" --transition-type fade --transition-step 90 --transition-duration 7
-  wal -i "$FILE" -n 
+  wal -i "$FILE" -n
 
   pkill -f rofi
-  cp ~/.cache/wal/colors-waybar.css ~/dotfiles/.config/waybar/colors1.css
-  cp ~/.cache/wal/colors.css ~/dotfiles/.config/mozilla/firefox/3rv9ix8a.dev-edition-default/chrome/colors1.css
-  cp colors1.css ~/dotfiles/.config/wlogout/colors1.css
-  cp ~/.cache/wal/colors-kitty.conf ~/dotfiles/.config/kitty/
+  cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors1.css
+  cp ~/.cache/wal/colors.css ~/.config/mozilla/firefox/3rv9ix8a.dev-edition-default/chrome/colors1.css
+  cp colors1.css ~/.config/wlogout/colors1.css
+  cp ~/.cache/wal/colors-kitty.conf ~/.config/kitty/
 
   "$HOME/dotfiles/.config/waybar/css_rasi.sh"
   mv colors1.rasi ~/dotfiles/.config/rofi/colors1.rasi
--- nix_conf/kitty.nix
+++ nix_conf/kitty.nix
@@ -4,8 +4,6 @@
  home.file."/.config/kitty/kitty.conf".text = ''
  include ~/dotfiles/.config/kitty/colors-kitty.conf
 
- uh huh
-
  copy_on_select yes
  
  enabled layout Tall, *
