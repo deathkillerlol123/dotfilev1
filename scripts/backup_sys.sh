eabfa5a3d9cfbab2bb31aec7af4e896672bc5cfc
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-03-09 22:48:09 +0000

    Update repository with latest changes

--- scripts/backup_sys.sh
+++ scripts/backup_sys.sh
@@ -1,16 +1,16 @@
 borg create --progress "/mnt/Backups/ArchB/BorgBackups2::$(date +%Y-%m-%d_%H-%M-%S)" \
-  --exclude '/home/nixboom/Downloads/*.iso' \
-  --exclude '/home/nixboom/Downloads/*.tar.gz' \
-  --exclude '/home/nixboom/Downloads/tmp*' \
-  --exclude '/home/nixboom/Documents/tmp*' \
-  --exclude '/home/nixboom/dotfiles/.git' \
-  --exclude '/home/nixboom/dotfiles/.gitignore' \
-  --exclude '/home/nixboom/Documents/codes/venv/' \
-  --exclude '/home/nixboom/Downloads/isoss/' \
-  --exclude '/home/nixboom/dotfiles/.mozilla' \
-  /home/nixboom/Documents \
-  /home/nixboom/Downloads \
-  /home/nixboom/Pictures/Screenshots \
-  /home/nixboom/dotfiles
+  --exclude '~/Downloads/*.iso' \
+  --exclude '~/Downloads/*.tar.gz' \
+  --exclude '~/Downloads/tmp*' \
+  --exclude '~/Documents/tmp*' \
+  --exclude '~/dotfiles/.git' \
+  --exclude '~/dotfiles/.gitignore' \
+  --exclude '~/Documents/codes/venv/' \
+  --exclude '~/Downloads/isoss/' \
+  --exclude '~/dotfiles/.mozilla' \
+  ~/Documents \
+  ~/Downloads \
+  ~/Pictures/Screenshots \
+  ~/dotfiles
 
 borg list /mnt/Backups/ArchB/BorgBackups2/
