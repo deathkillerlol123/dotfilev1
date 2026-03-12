d79331e1f2b754c36c8ac84bc651920b9ea1f2b9
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-03-12 23:08:50 +0000

    Update repository with latest changes

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -7,8 +7,8 @@ swaync &
 
 brightnessctl set 10%
 
-"$HOME/dotfiles/scripts/niri_monitors/placement.sh"
+"$HOME/dotfiles/scripts/niri_monitors/placement.sh" &
 
-"$HOME/dotfiles/scripts/colorschem/start_wall.sh"
+"$HOME/dotfiles/scripts/colorschem/start_wall.sh" &
 
-"$HOME/dotfiles/scripts/way.sh"
+"$HOME/dotfiles/scripts/way.sh" &
