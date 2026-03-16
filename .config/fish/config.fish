a9d4fdc73d596ecafe05829fff8ac5887122c656
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-03-16 10:37:48 +0000

    Update repository with latest changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,12 +8,9 @@ alias nrs='sudo nixos-rebuild switch --upgrade --flake ~/dotfiles/nixos'
 alias gu='~/dotfiles/scripts/git_updater.sh'
 alias up='~/dotfiles/scripts/updatesys_Nixos.sh'
 alias windows='~/dotfiles/scripts/Boot-Windows10.sh'
-
 alias total='gu && up'
-
 alias ls='eza -a --tree -L 1'
 alias zellij='zellij -l welcome'
-
 
 set -U fish_greeting
 starship init fish | source
