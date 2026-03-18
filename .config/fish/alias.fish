313fa6487bb406c7b0600a87a2d984bf1ccdefe2
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-03-18 18:56:21 +0000

    Update repository with latest changes

---
+++ .config/fish/alias.fish
@@ -0,0 +1,7 @@
+alias nrs='sudo nixos-rebuild switch --flake ~/dotfiles/nixos'
+alias gu='~/dotfiles/scripts/git_updater.sh'
+alias up='~/dotfiles/scripts/updatesys_Nixos.sh'
+alias windows='~/dotfiles/scripts/Boot-Windows10.sh'
+alias total='gu && up'
+alias ls='eza -a --tree -L 1'
+alias zellij='zellij -l welcome'
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,8 @@
-source ~/dotfiles/.config/fish/abbreviations.fish
+source ~/dotfiles/.config/fish/alias.fish
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-alias nrs='sudo nixos-rebuild switch --upgrade --flake ~/dotfiles/nixos'
-alias gu='~/dotfiles/scripts/git_updater.sh'
-alias up='~/dotfiles/scripts/updatesys_Nixos.sh'
-alias windows='~/dotfiles/scripts/Boot-Windows10.sh'
-alias total='gu && up'
-alias ls='eza -a --tree -L 1'
-alias zellij='zellij -l welcome'
 
 set -U fish_greeting
 starship init fish | source
