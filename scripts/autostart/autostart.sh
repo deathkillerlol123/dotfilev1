2dafbbd158f8310581762dcfa19421cecbcd17f5
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-03-20 21:34:51 +0000

    Update repository with latest changes

--- scripts/autostart/autostart.sh
+++ scripts/autostart/autostart.sh
@@ -5,10 +5,11 @@ eww daemon &
 copyq &
 swaync &
 
-brightnessctl set 10%
+brightnessctl set 10% &
 
 "$HOME/dotfiles/scripts/niri_monitors/placement.sh" &
 
 "$HOME/dotfiles/scripts/colorschem/start_wall.sh" &
 
 "$HOME/dotfiles/scripts/way.sh" &
+
