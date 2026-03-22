71bdfc4a6d009b1ada311654b6a1ddcff2439b68
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-03-22 18:02:01 +0000

    Update repository with latest changes

--- scripts/updatesys_Nixos.sh
+++ scripts/updatesys_Nixos.sh
@@ -6,7 +6,6 @@ sudo nixos-rebuild switch --upgrade --flake ~/dotfiles/nixos
 home-manager expire-generations -d
 nix store gc
 sudo nix store optimise
-home-manager remove-generations old
 
 nh clean all
 
