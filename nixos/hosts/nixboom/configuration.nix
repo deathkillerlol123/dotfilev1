8dd83f395be7e2f388da412e33a2aa4cfb536910
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-03-23 20:45:39 +0000

    Update repository with latest changes

--- nixos/hosts/nixboom/configuration.nix
+++ nixos/hosts/nixboom/configuration.nix
@@ -12,9 +12,34 @@ in
     "${mod}/login/"
     "${mod}/common/"
     "${mod}/sound/"
-    "${mod}/bootloader"
   ];
   boot = {
+    loader = {
+      efi.canTouchEfiVariables = true;
+      #systemd-boot.enable = true;
+      grub = {
+        enable = false;
+        devices = ["nodev"];
+       	efiSupport = true;
+       	useOSProber = true;
+       	theme = inputs.nixos-grub-themes.packages.${pkgs.system}.nixos;
+      };
+      limine = {
+        enable = true;
+        style = {
+          wallpapers = lib.filesystem.listFilesRecursive ../../themes/limine_wallpaper;        
+          wallpaperStyle = "centered";
+        };
+        extraEntries = ''
+          /Windows
+            protocol: efi
+            path: uuid(c5a64789-c514-43f3-97ce-48d094eead3c):/EFI/Microsoft/Boot/bootmgfw.efi
+        '';
+        extraConfig = ''
+          remember_last_entry: yes
+          '';
+      };
+    };
     kernelParams = [ "snd_hda_intel.dmic_detect=0" "snd_intel_dspcfg.dsp_driver=1"];
     kernelPackages = pkgs.linuxPackages_latest;
   };
