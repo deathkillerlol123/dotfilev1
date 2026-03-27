2c3ef5935cbdf4a5b3482ca9512af49ce6aa2ce8
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-03-27 22:10:36 +0000

    Update repository with latest changes

--- nixos/hosts/nixboom/configuration.nix
+++ nixos/hosts/nixboom/configuration.nix
@@ -39,9 +39,9 @@ in
             protocol: efi
             path: uuid(c5a64789-c514-43f3-97ce-48d094eead3c):/EFI/Microsoft/Boot/bootmgfw.efi
         '';
-        extraConfig = ''
-          remember_last_entry: yes
-          '';
+#        extraConfig = ''
+#          remember_last_entry: yes
+#          '';
       };
     };
     kernelParams = [ "snd_hda_intel.dmic_detect=0" "snd_intel_dspcfg.dsp_driver=1"];
