33437cd4ffa32026a609c60d6876aa342e5b1395
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-03-28 17:45:08 +0000

    Update repository with latest changes

--- nixos/modules/common/default.nix
+++ nixos/modules/common/default.nix
@@ -11,11 +11,7 @@
     gamescope
     gamemode
 
-    xdg-desktop-portal-hyprland
-    xdg-desktop-portal-wlr
     xdg-desktop-portal-gnome
-    xdg-desktop-portal-gtk
-    kdePackages.xdg-desktop-portal-kde
 
     uxplay
 
