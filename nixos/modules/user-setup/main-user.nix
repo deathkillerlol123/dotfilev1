bf5a51d2368374004e893e2103164f429faf3241
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-03-30 19:48:31 +0100

    Update repository with latest changes

--- nixos/modules/user-setup/main-user.nix
+++ nixos/modules/user-setup/main-user.nix
@@ -1,6 +1,7 @@
 {lib,config,pkgs,...}:
 {
   options  = {
+    main-user.enable = lib.mkOption "enable user module";
     main-user.userName = lib.mkOption {
       default = "mainuser";
       description = ''
@@ -8,8 +9,10 @@
       '';
     };
   };
-  users.users.${config.main-user.userName} = {
-    isNormalUser = true;
-    shell = pkgs.fish;
+  config = lib.mkIF config.main-user.enable {
+    users.users.${config.main-user.userName} = {
+      isNormalUser = true;
+      shell = pkgs.fish;
+    };
   };
 }
