63afee2a879343faee1255a91a798904acebbc30
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-03-30 20:10:53 +0100

    Update repository with latest changes

--- nixos/hosts/nixboom/configuration.nix
+++ nixos/hosts/nixboom/configuration.nix
@@ -56,7 +56,7 @@ in
     backupFileExtension = "backup";
     extraSpecialArgs = { inherit inputs ; };
       users = {
-        username = import ./home.nix;
+        ${username} = import ./home.nix;
       };
   };
   main-user.enable = true;
