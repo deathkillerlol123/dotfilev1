4995ee8692934d057b1f09b12cb2392c6f4e45e1
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-01 17:29:59 +0100

    Update repository with latest changes

--- nixos/nixos.nix
+++ nixos/nixos.nix
@@ -1,7 +1,7 @@
 { inputs, ...}: {
   flake = {
     nixosConfiguration.main = inputs.nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      systems = "x86_64-linux";
       specialArgs = {inherit inputs;};
       modules = [
         ./hosts/nixboom/configuration.nix
