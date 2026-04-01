02c32010c2db683940495fb77a59a480c169c22f
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-01 17:06:01 +0100

    Update repository with latest changes

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,10 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager = {
     	url = "github:nix-community/home-manager";
