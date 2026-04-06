ff6e72106caa30cd417421bc725e9a4702bab07c
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-06 23:04:03 +0100

    Update repository with latest changes

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -26,7 +26,7 @@
       		    system = "x86_64-linux";
       		    specialArgs = {inherit inputs;};
       		    modules = [
-			./modules/hosts/nixboom/myMachine.nix
+			./modules/hosts/nixboom/laptop.nix
       			inputs.home-manager.nixosModules.default
     		    ];
 		};
