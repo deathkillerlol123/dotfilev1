8b24183c0184a1d43023f922c9700531e9b76939
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-06 22:54:17 +0100

    Update repository with latest changes

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -26,6 +26,7 @@
       		    system = "x86_64-linux";
       		    specialArgs = {inherit inputs;};
       		    modules = [
+			./modules/hosts/nixboom/myMachine.nix
       			inputs.home-manager.nixosModules.default
     		    ];
 		};
@@ -38,8 +39,6 @@
 	];
 	imports = [
 	    ./modules/common/fonts.nix
-	    ./modules/hosts/nixboom/myMachine.nix
-	    ./modules/hosts/nixboom/hardware-configuration.nix
 	];
     };
 }
--- nixos/modules/hosts/nixboom/myMachine.nix
+++ nixos/modules/hosts/nixboom/myMachine.nix
@@ -5,59 +5,49 @@ let
   flake_pos="/home/nixboom/dotfiles/nixos/";
 in
 {
-  flake.nixosConfigurations.myMachine = inputs.nixpkgs.lib.nixosSystem {
-    modules = [
-      self.nixosModules.myMachinemodule
-      self.nixosModules.fonts
-      self.nixosModules.laptop-hardware
-    ];
-  };
-  flake.nixosModules.myMachineModule = {pkgs,...}: {
-    imports = [
-      ./hardware-configuration.nix
-      inputs.home-manager.nixosModules.default
-      ../main.nix    
-    ];
-    boot = {
-      loader = {
-        efi.canTouchEfiVariables = true;
-        limine = {
-          enable = true;
-          maxGenerations = 3;
-          style = {
-            wallpapers = lib.filesystem.listFilesRecursive ../../themes/limine_wallpaper;        
-          };
-          extraEntries = ''
-            /Windows
-              protocol: efi
-              path: uuid(c5a64789-c514-43f3-97ce-48d094eead3c):/EFI/Microsoft/Boot/bootmgfw.efi
-          '';
-  
-        };
-      };
-      kernelParams = [ "snd_hda_intel.dmic_detect=0" "snd_intel_dspcfg.dsp_driver=1" "quiet"];
-      kernelPackages = pkgs.linuxPackages_latest;
-      consoleLogLevel = 3;
-    };
-    main-user = {
-      enable = true;
-      userName = username;
-      shell = "fish";
-    };
-    users.users.${username}.extraGroups = ["wheel" "gamemode" "uinput"];
-    cleaner = {
-      enable = true;
-      flake-location = flake_pos;
-    };
-    home-manager = {
-      useUserPackages = true;
-      useGlobalPkgs = true;
-      backupFileExtension = "hm-backup";
-      extraSpecialArgs = { inherit inputs ; };
-        users = {
-          ${username} = import ./home.nix;
-        };
-    };
-    system.stateVersion = "25.11";
-  };
+ imports = [
+   ./hardware-configuration.nix
+   inputs.home-manager.nixosModules.default
+   ../main.nix    
+ ];
+ boot = {
+   loader = {
+     efi.canTouchEfiVariables = true;
+     limine = {
+       enable = true;
+       maxGenerations = 3;
+       style = {
+         wallpapers = lib.filesystem.listFilesRecursive ../../themes/limine_wallpaper;        
+       };
+       extraEntries = ''
+         /Windows
+           protocol: efi
+           path: uuid(c5a64789-c514-43f3-97ce-48d094eead3c):/EFI/Microsoft/Boot/bootmgfw.efi
+       '';
+     };
+   };
+   kernelParams = [ "snd_hda_intel.dmic_detect=0" "snd_intel_dspcfg.dsp_driver=1" "quiet"];
+   kernelPackages = pkgs.linuxPackages_latest;
+   consoleLogLevel = 3;
+ };
+ main-user = {
+   enable = true;
+   userName = username;
+   shell = "fish";
+ };
+ users.users.${username}.extraGroups = ["wheel" "gamemode" "uinput"];
+ cleaner = {
+   enable = true;
+   flake-location = flake_pos;
+ };
+ home-manager = {
+   useUserPackages = true;
+   useGlobalPkgs = true;
+   backupFileExtension = "hm-backup";
+   extraSpecialArgs = { inherit inputs ; };
+     users = {
+       ${username} = import ./home.nix;
+     };
+ };
+ system.stateVersion = "25.11";
 }
