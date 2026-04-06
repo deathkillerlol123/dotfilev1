b8289ca394a65f2f6356ba18bb977c4d71732fa7
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-06 17:34:57 +0100

    Update repository with latest changes

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -19,7 +19,7 @@
 
   outputs = { self, nixpkgs,flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; }
-    (top@{ config, withSystem, moduleWithSystem, ... }: {
+    (top@{ config, withSystem, moduleWithSystem, ... }:(inputs.import-tree ./modules); {
 	flake = {
 	    nixosConfigurations = {
       		nixbtw = nixpkgs.lib.nixosSystem {
