6d35a8cee861624e27354ef1429f64dfdb5b5cde
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-06 18:37:22 +0100

    Update repository with latest changes

--- nixos/nixos.nix
+++ nixos/nixos.nix
@@ -1,10 +1,20 @@
 {inputs, ...}:
 {
   flake = {
-    nixosConfiguration.main = inputs.nixpkgs.lib.nixosSystem {
-      modules = [
-        ./hosts/nixboom/configuration.nix
-      ];
+    nixosConfigurations = {
+      nixbtw = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+	specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/nixboom/configuration.nix
+	  inputs.home-manager.nixosModules.default
+        ];
+      };
     };
   };
+  systems = [
+    "x86_64-linux"
+    "aarch64-linux"
+      "aarch64-darwin"
+  ];
 }
