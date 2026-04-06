87f78a1babc6ab959d7104807db6b03aba81ceba
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-06 18:04:18 +0100

    Update repository with latest changes

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -19,26 +19,28 @@
 
   outputs = { self, nixpkgs,flake-parts, ... }@inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; }
-    (inputs.import-tree ./modules);
     (
-	top@{ config, withSystem, moduleWithSystem, ... }: {
-	    flake = {
-		nixosConfigurations = {
-      		    nixbtw = nixpkgs.lib.nixosSystem {
-      			system = "x86_64-linux";
-      			specialArgs = {inherit inputs;};
-      			modules = [
-      			    ./hosts/nixboom/configuration.nix
-      			    inputs.home-manager.nixosModules.default
-    			];
+	(inputs.import-tree ./modules);
+	(
+	    top@{ config, withSystem, moduleWithSystem, ... }: {
+		flake = {
+		    nixosConfigurations = {
+      			nixbtw = nixpkgs.lib.nixosSystem {
+      			    system = "x86_64-linux";
+      			    specialArgs = {inherit inputs;};
+      			    modules = [
+      				./hosts/nixboom/configuration.nix
+      				inputs.home-manager.nixosModules.default
+    			    ];
+			};
 		    };
 		};
-	    };
-	    systems = [
-		"x86_64-linux"
-		"aarch64-linux"
-		"aarch64-darwin"
-	    ];
-	}
-    );     
+		systems = [
+		    "x86_64-linux"
+		    "aarch64-linux"
+		    "aarch64-darwin"
+		];
+	    }
+	);
+    );
 }
