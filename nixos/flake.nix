742e6a7357e0f0df95ddf4b5a4f583539796361b
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-06 18:16:26 +0100

    Update repository with latest changes

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -18,7 +18,7 @@
   };
 
     outputs = inputs:
-    flake-parts.lib.mkFlake { inherit inputs; }
+      inputs.flake-parts.lib.mkFlake { inherit inputs; }
     (
 	top@{ config, withSystem, moduleWithSystem, ... }: {
 	    flake = {
