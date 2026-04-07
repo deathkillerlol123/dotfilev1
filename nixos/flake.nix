90b9434e2c73146b456c8654eecffbcebf6557c7
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-07 15:41:04 +0100

    Update repository with latest changes

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -17,7 +17,6 @@
       flake-parts.url = "github:hercules-ci/flake-parts";
       import-tree.url = "github:vic/import-tree";
   };
-
   outputs = { self, nixpkgs,flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; }
     {
