1dc89a9eb575550e859a267e937e92fbcb4a2b2f
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-08 12:46:19 +0100

    Update repository with latest changes

--- nixos/modules/hosts/nixboom/laptop.nix
+++ nixos/modules/hosts/nixboom/laptop.nix
@@ -1,6 +1,5 @@
-{ config, lib, pkgs, inputs, self, ... }:
-{
-   imports = [
+{ config, lib, pkgs, inputs, self, ... }:{
+   ximports = [
      inputs.home-manager.nixosModules.default
      ./laptop-hardware.nix
      ../main.nix
