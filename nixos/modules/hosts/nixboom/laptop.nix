8580cb5183beabb920a9b3be363bebd4179dd4dd
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-08 17:02:53 +0100

    Update repository with latest changes

--- nixos/modules/hosts/nixboom/laptop.nix
+++ nixos/modules/hosts/nixboom/laptop.nix
@@ -9,7 +9,10 @@
      generations = 3;
      wallpapers = ../../themes/limine_wallpaper;
      params = [ "snd_hda_intel.dmic_detect=0" "snd_intel_dspcfg.dsp_driver=1" "quiet"];
-     triangle.enable = true;
+     triangle = {
+       enable = true;
+       hello = "hells";
+     };
    };
    windows = {
      enable = true;
