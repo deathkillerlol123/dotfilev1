ad88b917a69285f8d12b504d92af2fff3872c5e4
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-09 16:23:25 +0100

    Update repository with latest changes

--- nixos/modules/settings/main-user.nix
+++ nixos/modules/settings/main-user.nix
@@ -1,54 +1,45 @@
 {lib,config,pkgs,...}:{
   options  = {
-    main-users = lib.mkOption {
-      default = {};
-      type = lib.types.attrsOf (lib.types.submodule ({ ... }: {
-        options = {
-          enable = lib.mkEnableOption "enable user module";
-          shell = lib.mkOption {
-            default = "bash";
-          };
-          flakelocation = lib.mkOption {
-            default = "/etc/nixos/";
-          };
-          groups = lib.mkOption {
-            default = ["wheel"];
-          };
-        };
-      }));
+    main-user.enable = lib.mkEnableOption "enable user module";
+    main-user = {
+      userName = lib.mkOption {
+        default = "mainuser";
+      };
+      shell = lib.mkOption {
+        default = "bash";
+      };
+      flakelocation = lib.mkOption {
+        default = "/etc/nixos/";
+      };
+      groups = lib.mkOption {
+        default = ["wheel"];
+      };
     };
   };
-
-  config = lib.mkMerge (
-    lib.mapAttrsToList (name: userCfg:
-      lib.mkIf userCfg.enable {
-        users.users.${name} = {
-          isNormalUser = true;
-          shell = pkgs.${userCfg.shell};
-          extraGroups = userCfg.groups;
-        };
-
-        security = {
-          sudo.extraRules = [{
-            users = [name];
-            commands = [{ command = "ALL";
-              options = ["NOPASSWD"];
-            }];
-          }];
-        };
-
-        programs = {
-          ${userCfg.shell}.enable = true;
-          nh = {
-            enable = true;
-            clean.enable = true;
-            flake = userCfg.flakelocation;
-          };
-          nix-ld = {
-            enable = true;
-          };
-        };
-      }
-    ) config.main-users
-  );
+  config = lib.mkIf config.main-user.enable {
+    users.users.${config.main-user.userName} = {
+      isNormalUser = true;
+      shell = pkgs.${config.main-user.shell};
+      extraGroups = config.main-user.groups;
+    };
+    security = {
+      sudo.extraRules = [{
+        users = [config.main-user.userName];
+        commands = [{ command = "ALL";
+          options = ["NOPASSWD"];
+        }];
+      }];
+    };
+    programs = {
+      ${config.main-user.shell}.enable = true;
+      nh = {
+        enable = true;
+	clean.enable = true;
+	flake = config.main-user.flakelocation;
+      };
+      nix-ld = {
+        enable = true;
+      };
+    };
+ };
 }
