4a849a945d596a96db8b36c2f7c139665c97c335
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-09 17:08:35 +0100

    Update repository with latest changes

--- nixos/modules/settings/main-user.nix
+++ nixos/modules/settings/main-user.nix
@@ -1,45 +1,62 @@
-{lib,config,pkgs,...}:{
-  options  = {
-    main-user.enable = lib.mkEnableOption "enable user module";
-    main-user = {
-      userName = lib.mkOption {
-        default = "mainuser";
-      };
-      shell = lib.mkOption {
-        default = "bash";
-      };
-      flakelocation = lib.mkOption {
-        default = "/etc/nixos/";
-      };
-      groups = lib.mkOption {
-        default = ["wheel"];
-      };
+{lib,config,pkgs,...}:
+{
+  options = {
+    main-users.enable = lib.mkEnableOption "enable users module";
+
+    main-users.users = lib.mkOption {
+      type = lib.types.listOf (lib.types.submodule {
+        options = {
+          userName = lib.mkOption {
+	    default = "user";
+	  };
+          shell = lib.mkOption {
+	    default = "bash";
+	  };
+          flakelocation = lib.mkOption {
+	    default = "/etc/nixos/";
+	  };
+          groups = lib.mkOption {
+	    default = ["wheel"];
+	  };
+        };
+      });
+      default = [];
+      description = "List of users to create";
     };
   };
-  config = lib.mkIf config.main-user.enable {
-    users.users.${config.main-user.userName} = {
-      isNormalUser = true;
-      shell = pkgs.${config.main-user.shell};
-      extraGroups = config.main-user.groups;
-    };
-    security = {
-      sudo.extraRules = [{
-        users = [config.main-user.userName];
-        commands = [{ command = "ALL";
-          options = ["NOPASSWD"];
-        }];
+  config = lib.mkIf config.main-users.enable {
+    users.users = lib.listToAttrs (map (u: {
+      name = u.userName;
+      value = {
+        isNormalUser = true;
+        shell = pkgs.${u.shell};
+        extraGroups = u.groups;
+      };
+    }) config.main-users.users);
+  
+    security.sudo.extraRules = lib.concatMap (u: [{
+      users = [u.userName];
+      commands = [{
+        command = "ALL";
+        options = ["NOPASSWD"];
       }];
-    };
-    programs = {
-      ${config.main-user.shell}.enable = true;
-      nh = {
+    }]) config.main-users.users;
+  
+    programs = lib.listToAttrs (map (u: {
+      name = u.shell;
+      value = { enable = true; };
+    }) config.main-users.users);
+  
+    # The nh program for each user (optional, keeps original style)
+    nh = lib.listToAttrs (map (u: {
+      name = u.userName;
+      value = {
         enable = true;
-	clean.enable = true;
-	flake = config.main-user.flakelocation;
+        clean.enable = true;
+        flake = u.flakelocation;
       };
-      nix-ld = {
-        enable = true;
-      };
-    };
- };
+    }) config.main-users.users);
+  
+    nix-ld.enable = true;
+  };
 }
