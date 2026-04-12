3b9db832e285e28784b63783aaabf8dc810cc352
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-12 18:07:41 +0100

    Update repository with latest changes

--- nixos/modules/environment/default.nix
+++ nixos/modules/environment/default.nix
@@ -2,7 +2,7 @@
   services = {
     desktopManager = {
       cosmic.enable = false;
-      plasma6.enable = true; #KDE plasma cannot be true with gnome
+      plasma6.enable = false; #KDE plasma cannot be true with gnome
       gnome = {
         enable = false;
       };
