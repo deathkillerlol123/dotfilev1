bf5fbb98173970f24d77338d4f7ee8014f01f9ac
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-18 00:14:48 +0100

    Fri Apr 17 11:14:48 PM UTC 2026

--- nixos/modules/applications/synchthing.nix
+++ nixos/modules/applications/synchthing.nix
@@ -3,6 +3,10 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
+    settings.gui = {
+      user = "nixboom";
+      password = "expire";
+    };
   };
   networking.firewall.allowedTCPPorts = [ 8384 ];
 }
