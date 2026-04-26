34bfc59d043e7846eece07c292c0283ce3f9a7d8
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-26 23:38:00 +0100

    Sun Apr 26 11:38:00 PM BST 2026

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -17,5 +17,15 @@
 	    "aarch64-linux"
 	    "aarch64-darwin"
 	];
+	nixosConfigurations = {
+            nixbtw = nixpkgs.lib.nixosSystem {
+		system = "x86_64-linux";
+                specialArgs = {inherit inputs;};
+                modules = [
+		    ./modules/hosts/nixboom/laptop.nix
+                    inputs.home-manager.nixosModules.default
+                ];
+            };
+	};
     };
 }
