0c3344de0cf6a17b6fa49dcd89248b05b93286bb
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-26 23:51:19 +0100

    Sun Apr 26 11:51:19 PM BST 2026

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -22,7 +22,6 @@
                   system = "x86_64-linux";
                   specialArgs = {inherit inputs;};
                   modules = [
-                    ./modules/hosts/nixboom/laptop.nix
                     inputs.home-manager.nixosModules.default
                   ];
 	      };
