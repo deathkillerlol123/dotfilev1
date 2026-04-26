a5ddd088c57ed36f9a0950081e31aa5d0d334799
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-26 23:09:07 +0100

    Sun Apr 26 11:09:07 PM BST 2026

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -11,7 +11,7 @@
   outputs = { self, nixpkgs,flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; }  
     {
-	imports = builtins.attrValues (inputs.import-tree ./modules);
+	imports = [];
 	flake = {
 	    nixosConfigurations = {
       		nixbtw = nixpkgs.lib.nixosSystem {
