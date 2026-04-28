76dc759bcf8ae762020314b325eede4707ff1f48
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-28 18:54:58 +0100

    Tue Apr 28 06:54:58 PM BST 2026

--- nixos/modules/common/software.nix
+++ nixos/modules/common/software.nix
@@ -3,17 +3,16 @@
     environment.systemPackages = with pkgs; [
       wdisplays   
       firefox-devedition
-      qutebrowser
       whatsapp-electron
       discord
-  
       copyq
       grim
       slurp
       swappy
       wl-clipboard
-  #    qbittorrent
-  #    ladybird    
+      wezterm
+  #    qbittorrent qutebrowser
+
    ];
   };
 }
--- nixos/modules/common/system.nix
+++ nixos/modules/common/system.nix
@@ -15,7 +15,6 @@
       xdg-desktop-portal-wlr
       wlr-randr
 
-      wezterm
       starship
       bash
       fish
@@ -35,9 +34,7 @@
       statix      
     ];
     fonts.packages = with pkgs; [
-      nerd-fonts.fira-code
-      nerd-fonts.droid-sans-mono
-      nerd-fonts.dejavu-sans-mono
+      nerd-fonts
     ];
   };
 }
