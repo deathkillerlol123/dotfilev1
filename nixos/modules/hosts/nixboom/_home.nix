0925cde6d4ec1d1aee8aa7c56ef573cf92520751
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-28 23:38:09 +0100

    Tue Apr 28 11:38:09 PM BST 2026

--- nixos/modules/hosts/nixboom/_home.nix
+++ nixos/modules/hosts/nixboom/_home.nix
@@ -3,6 +3,7 @@ let
   dotfiles =  config.lib.file.mkOutOfStoreSymlink "/home/nixboom/dotfiles";
   firefox_user = "hcq4c6te.dev-edition-default";
   user = "nixboom";
+  conf ="${dotfiles}/.config";
 in
 {
   home = {
@@ -16,29 +17,27 @@ in
   };
   programs.home-manager.enable = true;  
   home.file = {
+    ".config/mozilla/firefox/${firefox_user}/chrome/userChrome.css" = {source = "${dotfiles}/.config/mozilla/userChrome.css";};      
     ".zshrc" = {source = "${dotfiles}/.zshrc";};
-    ".wezterm.lua" = {source = "${dotfiles}/.wezterm.lua";};    
-    ".config/fish" = {source = "${dotfiles}/.config/fish"; recursive = true;};    
-    ".config/waybar" = {source = "${dotfiles}/.config/waybar"; recursive = true; };
-    ".config/walrus" = {source = "${dotfiles}/.config/walrus"; recursive = true;};
-    ".config/wallust" = {source = "${dotfiles}/.config/wallust"; recursive = true;};
-    ".config/swaync" = {source = "${dotfiles}/.config/swaync"; recursive = true;};
-    ".config/swaylock" = {source = "${dotfiles}/.config/swaylock";};
-    ".config/rofi" = {source = "${dotfiles}/.config/rofi"; recursive = true;};
-    ".config/nvim" = {source = "${dotfiles}/.config/nvim"; recursive = true;};
-    ".config/niri" = {source = "${dotfiles}/.config/niri"; recursive = true;};
-    ".config/fastfetch" = {source = "${dotfiles}/.config/fastfetch";};
-    ".config/mango" = {source = "${dotfiles}/.config/mango";};
-    ".config/mako" = {source = "${dotfiles}/.config/mako"; recursive = true;};
-    ".config/eww" = {source = "${dotfiles}/.config/eww"; recursive = true;};
-    ".config/quickshell" = {source = "${dotfiles}/.config/quickshell"; recursive = true;};  
+    ".emacs" = {source = "${dotfiles}/.emacs";};    
+    ".wezterm.lua" = {source = "${dotfiles}/.wezterm.lua";};
+    ".config/mango" = {source = "${conf}/mango";};    
+    ".config/swaylock" = {source = "${conf}/swaylock";};
+    ".config/fastfetch" = {source = "${conf}/fastfetch";};
     ".config/starship.toml" = {source = "${dotfiles}/.config/starship.toml";};
+    ".config/fish" = {source = "${conf}/fish"; recursive = true;};    
+    ".config/waybar" = {source = "${conf}/waybar"; recursive = true;};
+    ".config/walrus" = {source = "${conf}/walrus"; recursive = true;};
+    ".config/wallust" = {source = "${conf}/wallust"; recursive = true;};
+    ".config/swaync" = {source = "${conf}/swaync"; recursive = true;};
+    ".config/rofi" = {source = "${conf}/rofi"; recursive = true;};
+    ".config/nvim" = {source = "${conf}/nvim"; recursive = true;};
+    ".config/niri" = {source = "${conf}/niri"; recursive = true;};
+    ".config/mako" = {source = "${conf}/mako"; recursive = true;};
+    ".config/eww" = {source = "${conf}/eww"; recursive = true;};
+    ".config/quickshell" = {source = "${conf}/quickshell"; recursive = true;};
+    ".config/qutebrowser" = {source = "${conf}/qutebrowser"; recursive = true;};    
     ".local/share/applications" = {source = "${dotfiles}/.local/share/applications"; recursive = true;};
-    ".config/qutebrowser" = {source = "${dotfiles}/.config/qutebrowser"; recursive = true;};
-    ".emacs" = {source = "${dotfiles}/.emacs";};
-    ".config/mozilla/firefox/${firefox_user}/chrome/userChrome.css" = {
-      source = "${dotfiles}/.config/mozilla/userChrome.css";
-    };
   };
   gtk = {
     enable = true;
