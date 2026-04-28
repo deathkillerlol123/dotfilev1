df18ebc33802ea4929230da9595426a97d470240
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-28 18:08:36 +0100

    Tue Apr 28 06:08:36 PM BST 2026

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -15,10 +15,10 @@
 	    (inputs.import-tree ./modules)
 	];
 	systems = [
-	    "x86_64-linux";
-	    "aarch64-linux";
-	    "x86_64-darwin";
-	    "aarch64-darwin";
+	    "x86_64-linux"
+	    "aarch64-linux"
+	    "x86_64-darwin"
+	    "aarch64-darwin"
 	];
 	flake = {
 	    nixosModules ={};
