5c251f724a09fb02a4112159928512e4e04c005d
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-29 17:02:00 +0100

    Wed Apr 29 05:02:00 PM BST 2026

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
       nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-      nixos-hardware.url = "github:NixOS/nixos-hardware/master";      
       nur = {
         url = "github:nix-community/NUR";
         inputs.nixpkgs.follows = "nixpkgs";
