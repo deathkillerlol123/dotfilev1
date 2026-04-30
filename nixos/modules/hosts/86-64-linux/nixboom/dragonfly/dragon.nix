50502926acceca27492d09940299203ba8723982
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-04-30 22:43:00 +0100

    Thu Apr 30 10:43:00 PM BST 2026

--- nixos/modules/hosts/86-64-linux/nixboom/dragonfly/dragon.nix
+++ nixos/modules/hosts/86-64-linux/nixboom/dragonfly/dragon.nix
@@ -32,7 +32,7 @@ in
     home-man = {
       enable = true;
       ext = "hm-backup";
-      home = self.nixosModules.boomhome;
+      home = "${self.outPath}/modules/hosts/86-64-linux/${user}/dragonfly/_homedragboom.nix";
     };
     nix.settings.trusted-users = [ "root" user ];
     system.stateVersion = "25.11";
