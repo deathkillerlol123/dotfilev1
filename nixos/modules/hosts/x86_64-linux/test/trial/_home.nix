2306c9d96cce7729cc68fe4d30b95cab5234c599
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-05-01 23:54:27 +0100

    Fri May  1 11:54:27 PM BST 2026

---
+++ nixos/modules/hosts/x86_64-linux/test/trial/_home.nix
@@ -0,0 +1,67 @@
+{config, pkgs,inputs, ... }:
+let
+  dotfiles =  config.lib.file.mkOutOfStoreSymlink "/home/nixboom/dotfiles";
+  firefox_user = "hcq4c6te.dev-edition-default";
+  user = "nixboom";
+  conf ="${dotfiles}/.config";
+  mkFile = name: {
+    inherit name;
+    value = {
+      source = "${dotfiles}/${name}";
+    };
+  };    
+  mkconf = name: {
+    name = ".config/${name}";
+    value = {
+      source = "${conf}/${name}";
+      recursive = true;
+    };
+  };
+  mkdirs = path: {
+    name = path;
+    value = {
+      source = "${dotfiles}/${path}";
+      recursive = true;
+    };
+  };  
+  configs = builtins.listToAttrs [
+    (mkconf "mango")
+    (mkconf "swaylock")
+    (mkconf "fastfetch")
+    (mkconf "starship.toml")
+    (mkconf "fish")
+    (mkconf "waybar")
+    (mkconf "walrus")
+    (mkconf "wallust")
+    (mkconf "swaync")
+    (mkconf "rofi")
+    (mkconf "nvim")
+    (mkconf "niri")
+    (mkconf "mako")
+    (mkconf "eww")
+    (mkconf "quickshell")
+    (mkconf "qutebrowser")
+    (mkFile ".zshrc")
+    (mkFile ".emacs")
+    (mkFile ".wezterm.lua")
+    (mkdirs ".local/share/applications")
+  ];
+in
+{
+  home = {
+    username = user;
+    homeDirectory = "/home/${user}";
+    stateVersion = "25.11"; # Please read the comment before changing.    
+  };
+  programs.home-manager.enable = true;  
+  home.file = {
+    ".config/mozilla/firefox/${firefox_user}/chrome/userChrome.css" = {source = "${dotfiles}/.config/mozilla/userChrome.css"; };    
+  } // configs;
+  gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.orchis-theme;
+      name = "Orchis-Grey-Dark";
+    };
+  };
+}
