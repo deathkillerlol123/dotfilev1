ed14f9bcfb2e6a21406378726b1b03367c332473
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-05-05 22:27:49 +0100

    Tue May  5 10:27:49 PM BST 2026

--- nixos/modules/settings/main-user.nix
+++ nixos/modules/settings/main-user.nix
@@ -1,20 +1,18 @@
 {inputs,self,...}:{
   flake.nixosModules.main-user = {lib,config,pkgs,...}:{
-    options  = {
-      main-user.enable = lib.mkEnableOption "enable user module";
-      main-user = {
-        username = lib.mkOption {
-          default = "nixboom";
-        };
-        shell = lib.mkOption {
-          default = "fish";
-        };
-        flakelocation = lib.mkOption {
-          default = "${self.outPath}";
-        };
-        groups = lib.mkOption {
-          default = ["wheel"];
-        };	
+    options.main-user  = {
+      enable = lib.mkEnableOption "enable user module";
+      username = lib.mkOption {
+        default = "nixboom";
+      };
+      shell = lib.mkOption {
+        default = "fish";
+      };
+      flakelocation = lib.mkOption {
+        default = "${self.outPath}";
+      };
+      groups = lib.mkOption {
+        default = ["wheel"];
       };
     };
     config = {
