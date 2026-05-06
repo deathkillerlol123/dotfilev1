8efdc1ba98502d20401cd24814648656c07fb566
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-05-06 23:23:25 +0100

    Wed May  6 11:23:25 PM BST 2026

--- nixos/modules/hosts/users/nixboom/config.nix
+++ nixos/modules/hosts/users/nixboom/config.nix
@@ -27,6 +27,10 @@ in
       };
     };    
     flatpak.apps = ["org.vinegarhq.Sober" ];
+    services.flatpak.packages = [
+      { appId = "com.brave.Browser"; origin = "flathub";  }
+      "ch.openboard.OpenBoard"
+    ];    
     system.stateVersion = "25.11";
   };
 }
