7cf3e44381cc311924593b08de0eae8c5fcbe9bb
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-05-12 17:14:21 +0100

    Tue May 12 05:14:21 PM BST 2026

---
+++ scripts/system_updaters/final.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+"$HOME/dotfiles/scripts/system_updaters/updatesys_Nixos.sh" &&
+
+shutdown now 
--- scripts/system_updaters/updatesys_Nixos.sh
+++ scripts/system_updaters/updatesys_Nixos.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env fish
 
 gu
-
 sudo nixos-rebuild switch --upgrade --flake ~/dotfiles/nixos
-
 nix store gc
 sudo nix store optimise
-
-
 nh clean all --keep 3
-
 rm -rf ~/.cache/*
-
 gu
