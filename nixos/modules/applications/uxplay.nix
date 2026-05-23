0158c9acbbf6f6798320912f8eb7b5299a673c57
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-05-23 12:21:45 +0100

    Sat May 23 12:21:45 PM BST 2026

--- nixos/modules/applications/uxplay.nix
+++ nixos/modules/applications/uxplay.nix
@@ -7,18 +7,19 @@
     services.avahi = {
       enable = true;
         nssmdns4 = true;
-          publish = {
-            enable = true;
-            addresses = true;
-            hinfo = true;
-            userServices = true;
-            workstation = true;
+        publish = {
+          enable = true;
+          addresses = true;
+          hinfo = true;
+          userServices = true;
+          workstation = true;
+	  domain = true;
       };
     };
     networking.firewall = {
       enable = true;
-      allowedTCPPorts = [ 7000 7100 7001 62078 ];
-      allowedUDPPorts = [ 5353 6000 7011 ];
+      allowedTCPPorts = [ 7000 7100 7001 ];
+      allowedUDPPorts = [ 5353 6000 6001 7011 ];
     };
   };
 }
