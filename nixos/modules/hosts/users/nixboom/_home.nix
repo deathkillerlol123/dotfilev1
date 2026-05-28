bf4180fb6df2dbe6b46434c2726b0642aa8e9a5e
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-05-28 17:53:59 +0100

    Thu May 28 05:53:59 PM BST 2026

--- nixos/modules/hosts/users/nixboom/_home.nix
+++ nixos/modules/hosts/users/nixboom/_home.nix
@@ -1,31 +1,41 @@
 { config, pkgs, lib, inputs, ... }:
+
 let
   dotfiles =
     config.lib.file.mkOutOfStoreSymlink "/home/nixboom/dotfiles";
   firefox_user = "hcq4c6te.dev-edition-default";
   user = "nixboom";
   conf = "${dotfiles}/.config";
+
   mkFile = names:
     lib.genAttrs names (name: {
       source = "${dotfiles}/${name}";
     });
+
   mkconf = names:
-    lib.genAttrs names (name: {
-      source = "${conf}/${name}";
-      recursive = true;
-    });
+    lib.listToAttrs (map (name: {
+      name = ".config/${name}";
+      value = {
+        source = "${conf}/${name}";
+        recursive = true;
+      };
+    }) names);
+
   mkdirs = paths:
     lib.genAttrs paths (path: {
       source = "${dotfiles}/${path}";
       recursive = true;
     });
+
 in {
   home = {
     username = user;
     homeDirectory = "/home/${user}";
     stateVersion = "25.11";
   };
+
   programs.home-manager.enable = true;
+
   home.file =
     {
       ".config/mozilla/firefox/${firefox_user}/chrome/userChrome.css" = {
@@ -58,6 +68,7 @@ in {
     // (mkdirs [
       ".local/share/applications"
     ]);
+
   gtk = {
     enable = true;
     theme = {
