585482e34055640c45e0f105ee6a0b9e4a76277f
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-05-28 17:57:04 +0100

    Thu May 28 05:57:04 PM BST 2026

--- nixos/modules/hosts/users/nixboom/_home.nix
+++ nixos/modules/hosts/users/nixboom/_home.nix
@@ -1,17 +1,14 @@
 { config, pkgs, lib, inputs, ... }:
-
 let
   dotfiles =
     config.lib.file.mkOutOfStoreSymlink "/home/nixboom/dotfiles";
   firefox_user = "hcq4c6te.dev-edition-default";
   user = "nixboom";
   conf = "${dotfiles}/.config";
-
   mkFile = names:
     lib.genAttrs names (name: {
       source = "${dotfiles}/${name}";
     });
-
   mkconf = names:
     lib.listToAttrs (map (name: {
       name = ".config/${name}";
@@ -20,55 +17,32 @@ let
         recursive = true;
       };
     }) names);
-
   mkdirs = paths:
     lib.genAttrs paths (path: {
       source = "${dotfiles}/${path}";
       recursive = true;
     });
-
 in {
   home = {
     username = user;
     homeDirectory = "/home/${user}";
     stateVersion = "25.11";
   };
-
   programs.home-manager.enable = true;
-
   home.file =
     {
       ".config/mozilla/firefox/${firefox_user}/chrome/userChrome.css" = {
         source = "${dotfiles}/.config/mozilla/userChrome.css";
       };
     }
-    // (mkconf [
-      "mango"
-      "swaylock"
-      "fastfetch"
-      "fish"
-      "waybar"
-      "walrus"
-      "wallust"
-      "swaync"
-      "rofi"
-      "nvim"
-      "niri"
-      "mako"
-      "eww"
-      "quickshell"
-      "qutebrowser"
+    // (mkconf ["mango" "swaylock" "fastfetch" "fish" "waybar" "walrus" "wallust" "swaync"
+                "rofi" "nvim" "niri" "mako" "eww" "quickshell" "qutebrowser"
     ])
-    // (mkFile [
-      ".zshrc"
-      ".emacs"
-      ".wezterm.lua"
-      ".config/starship.toml"
+    // (mkFile [".zshrc" ".emacs" ".wezterm.lua" ".config/starship.toml"
     ])
     // (mkdirs [
       ".local/share/applications"
     ]);
-
   gtk = {
     enable = true;
     theme = {
