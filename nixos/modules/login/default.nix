527448c1ce2a9fe5d2b3be9d6c9e49be10e39a39
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-05-28 17:07:56 +0100

    Thu May 28 05:07:56 PM BST 2026

--- nixos/modules/login/default.nix
+++ nixos/modules/login/default.nix
@@ -3,7 +3,7 @@
     services = {
       displayManager = {
         sddm = {
-          enable = false;
+          enable = true;
           wayland.enable = true;
           package = pkgs.kdePackages.sddm;
           theme = "sddm-astronaut-theme";
@@ -13,7 +13,7 @@
           wayland = true;
         };
         ly = {
-          enable = true;
+          enable = false;
           settings = {
             clear_screen = true;
           };
