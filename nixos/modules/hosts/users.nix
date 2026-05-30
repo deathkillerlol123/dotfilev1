2df201e4b2ca6269ef16da8134656f90ef395e81
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-05-30 16:27:05 +0100

    Sat May 30 04:27:05 PM BST 2026

--- nixos/modules/hosts/users.nix
+++ nixos/modules/hosts/users.nix
@@ -1,9 +1,5 @@
-{inputs,self,...}:
-let
-  m = "flake.nixosModules";
-in
-{
-  ${m}.users = { config, lib, pkgs, inputs,... }:{
+{inputs,self,...}:{
+  flake.nixosModules.users = { config, lib, pkgs, inputs,... }:{
     imports = with self.nixosModules; [
       main-user
       nixboom
