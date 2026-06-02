0d7b60f81fe153e45e2e98310deb5b7784429a22
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-02 19:18:57 +0100

    Tue Jun  2 07:18:57 PM BST 2026

--- nixos/modules/custom/windows.nix
+++ nixos/modules/custom/windows.nix
@@ -1,24 +1,24 @@
-{ config, pkgs, ... }:
-
-{
-  environment.systemPackages = [
-    pkgs.efibootmgr
-    (pkgs.writeShellScriptBin "reboot-windows" ''
-      set -euo pipefail
-
-      entry=$(
-        efibootmgr |
-        grep -i "windows" |
-        head -n1 |
-        sed -E 's/^Boot[0-9A-Fa-f]+\\*? //'
-      )
-
-      if [ -z "$entry" ]; then
-        echo "No Windows EFI boot entry found"
-        exit 1
-      fi
-
-      exec systemctl reboot --boot-loader-entry="$entry"
-    '')
-  ];
+{ config, pkgs, ... }:{
+  flake.nixosModules.bluescreen = {config,pkgs,inputs,lib,...}:{
+    environment.systemPackages = [
+      pkgs.efibootmgr
+      (pkgs.writeShellScriptBin "reboot-windows" ''
+        set -euo pipefail
+  
+        entry=$(
+          efibootmgr |
+          grep -i "windows" |
+          head -n1 |
+          sed -E 's/^Boot[0-9A-Fa-f]+\\*? //'
+        )
+  
+        if [ -z "$entry" ]; then
+          echo "No Windows EFI boot entry found"
+          exit 1
+        fi
+  
+        exec systemctl reboot --boot-loader-entry="$entry"
+      '')
+    ];
+  };
 }
