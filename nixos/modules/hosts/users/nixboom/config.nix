c053bfe97ac1a3df2c43de2c6ef4125803957206
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-02 18:56:15 +0100

    Tue Jun  2 06:56:15 PM BST 2026

--- nixos/modules/hosts/users/nixboom/config.nix
+++ nixos/modules/hosts/users/nixboom/config.nix
@@ -6,7 +6,7 @@ in
 {
   flake.nixosModules.nixboom = { config, lib, pkgs, inputs,... }:{
     imports = with self.nixosModules; [
-      uxplay fish bluescreen
+      uxplay fish
       dragware main
     ];
     home-man = {
