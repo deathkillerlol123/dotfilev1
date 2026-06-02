b90225ca2d0572160e3103d4cc926b96b3e9f5d3
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-02 18:26:54 +0100

    Tue Jun  2 06:26:54 PM BST 2026

--- scripts/Boot-Windows10.sh
+++ scripts/Boot-Windows10.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-sudo efibootmgr -n 0001
+BOOTNUM=$(efibootmgr | grep -i "Windows Boot Manager" | sed -E 's/^Boot([0-9A-Fa-f]+).*/\1/')
+sudo efibootmgr -n "$BOOTNUM"
 sudo reboot
