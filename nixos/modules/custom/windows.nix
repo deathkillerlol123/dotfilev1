d769de5de25eafb41a601cffe33626dfe72d4a99
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-02 19:26:48 +0100

    Tue Jun  2 07:26:48 PM BST 2026

--- nixos/modules/custom/windows.nix
+++ nixos/modules/custom/windows.nix
@@ -1,17 +1,21 @@
-{ config, pkgs, ... }:{
+{ config, pkgs, ... }:
+let
+  windows-session = pkgs.writeTextFile {
+    name = "windows-session";
+    destination = "/share/wayland-sessions/windows.desktop";
+    text = ''
+      [Desktop Entry]
+      Name=Windows
+      Comment=Reboot into Windows
+      Exec=systemctl reboot --boot-loader-entry=Windows Boot Manager
+      Type=Application
+    '';
+};
+in
+{
   flake.nixosModules.bluescreen = {config,pkgs,inputs,lib,...}:{
     environment.systemPackages = [
-      (pkgs.writeTextFile {
-        name = "windows-session";
-        destination = "/share/wayland-sessions/windows.desktop";
-        text = ''
-          [Desktop Entry]
-          Name=Windows
-          Comment=Reboot into Windows
-          Exec=systemctl reboot --boot-loader-entry=Windows Boot Manager
-          Type=Application
-        '';
-      })
+      windows-session
     ];
   };
 }
