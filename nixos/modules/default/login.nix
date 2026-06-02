cdab27d8de93e021ae1d28c6d571a3f468299f5b
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-02 18:19:10 +0100

    Tue Jun  2 06:19:10 PM BST 2026

--- nixos/modules/default/login.nix
+++ nixos/modules/default/login.nix
@@ -19,6 +19,21 @@
             clear_screen = true;
           };
         };
+	sessionPackages = [
+	  (pkgs.writeTextFile {
+	    name = "windows-session";
+	    destination = "/share/xsessions/windows.desktop";
+	    text = ''
+              [Desktop Entry]
+              Name=Windows
+              Comment=Boot Windows
+              Exec=/home/nixboom/dotfiles/scripts/Boot-Windows10.sh
+              Type=Application
+	    '';
+	} // {
+	  providedSessions = [ "windows" ];
+	  })
+	];
       };
       xserver = {
         enable = true;
