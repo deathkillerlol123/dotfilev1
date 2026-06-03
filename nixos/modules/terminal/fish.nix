27b30e5967c9c384ddd1b50513f42131bd1fd419
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-03 12:52:48 +0100

    Wed Jun  3 12:52:48 PM BST 2026

--- nixos/modules/terminal/fish.nix
+++ nixos/modules/terminal/fish.nix
@@ -4,10 +4,10 @@
     programs.fish = {
       enable = true;
       shellAliases = {
-        gu="~/dotfiles/scripts/git_updater.sh";
-  	n="gu && sudo nixos-rebuild switch --flake ~/dotfiles/nixos";
-  	up="~/dotfiles/scripts/system_updaters/updatesys_Nixos.sh";
-  	w="~/dotfiles/scripts/Boot-Windows10.sh";
+        gu="~/dotfiles/nixos/scripts/git_updater.sh";
+	up="~/dotfiles/nixos/scripts/system_updaters/updatesys_Nixos.sh";
+  	w="~/dotfiles/nixos/scripts/Boot-Windows10.sh";
+  	n="gu && sudo nixos-rebuild switch --flake ~/dotfiles/nixos";	
   	t="gu && up";
   	ls="eza -a --tree -L 1";
   	e="emacs -nw";
