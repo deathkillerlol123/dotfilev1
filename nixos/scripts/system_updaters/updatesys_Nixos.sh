9665cef8dcf9617149634b3b0c32b1d00324e3e6
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-03 12:55:49 +0100

    Wed Jun  3 12:55:49 PM BST 2026

--- nixos/scripts/system_updaters/updatesys_Nixos.sh
+++ nixos/scripts/system_updaters/updatesys_Nixos.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env fish
 
-~/dotfiles/scripts/git_updater.sh
+~/dotfiles/nixos/scripts/git_updater.sh
 sudo nixos-rebuild switch --upgrade --flake ~/dotfiles/nixos
 nix store gc
 sudo nix store optimise
 nh clean all --keep 3
 rm -rf ~/.cache/*
-~/dotfiles/scripts/git_updater.sh
+~/dotfiles/nixos/scripts/git_updater.sh
