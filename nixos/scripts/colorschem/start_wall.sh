0969d6faeb60f6c8a0faecc14d2bb067acbd7339
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-08 11:31:23 +0100

    Mon Jun  8 11:31:23 AM BST 2026

--- nixos/scripts/colorschem/start_wall.sh
+++ nixos/scripts/colorschem/start_wall.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd ~/dotfiles/nixos/themes/boot_wallpaper/
+cd ~/dotfiles/nixos/bootloader/
 value="$(ls)"
 swww img "$value" --transition-type fade --transition-step 90 --transition-duration 7 &
