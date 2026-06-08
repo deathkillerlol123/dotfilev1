096fb7083c4b9262e432fa7d29e4ed59251f9ce3
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-08 11:21:46 +0100

    trial4

--- nixos/dots/nixboom/.config/waybar/random_wallpaper.sh
+++ nixos/dots/nixboom/.config/waybar/random_wallpaper.sh
@@ -21,8 +21,8 @@ if [ -n "$FILE" ]; then
   mv colors.rasi "${OUPUT}/rofi/colors.rasi"
   pkill -f rofi
 
-  cd ~/dotfiles/nixos/themes/
+  cd ~/dotfiles/nixos/bootloader/
   rm -rf *
-  cp "$FILE" "$HOME/dotfiles/nixos/themes/"
+  cp "$FILE" "$HOME/dotfiles/nixos/bootloader/"
 
 fi
