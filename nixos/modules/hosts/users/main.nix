fbf09705c7682ec8ac1b6c8271dfa93a31ce33da
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-14 20:49:04 +0100

    Sun Jun 14 08:49:04 PM BST 2026

--- nixos/modules/hosts/users/main.nix
+++ nixos/modules/hosts/users/main.nix
@@ -13,7 +13,6 @@
     documentation.enable = false;
     boot = {
       tmp.cleanOnBoot = true;    
-      kernelPackages = pkgs.linuxPackages_latest;
       consoleLogLevel = 3;
       kernelParams = [
         "zswap.enabled=1"
