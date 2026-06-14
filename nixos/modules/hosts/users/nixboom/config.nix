26b79c0263de936fc8129721ad608b60dded43a3
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-14 21:41:34 +0100

    Sun Jun 14 09:41:34 PM BST 2026

--- nixos/modules/hosts/users/nixboom/config.nix
+++ nixos/modules/hosts/users/nixboom/config.nix
@@ -31,7 +31,6 @@ in
     services.flatpak.packages = [
       { appId = "com.brave.Browser"; origin = "flathub";  }
       "com.obsproject.Studio"
-      "im.riot.Riot"
     ];
     programs.mangowc.enable = true;
     networking.firewall = {
