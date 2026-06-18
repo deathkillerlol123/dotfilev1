c8b0f9a43f2fb3486ebae26a05920bc2280d0a29
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-18 23:44:02 +0100

    Thu Jun 18 11:44:02 PM BST 2026

---
+++ nixos/modules/hosts/users/doct/_home.nix
@@ -0,0 +1,23 @@
+{config,pkgs,lib,inputs,...}:
+let
+  user = "doct";
+  dotfiles = config.lib.file.mkOutOfStoreSymlink "/home/${user}/dotfiles/nixos/dots/${user}/";
+  conf = "${dotfiles}/.config";
+in
+{
+  imports = [
+    inputs.nix-flatpak.homeManagerModules.nix-flatpak
+  ];
+  services.flatpak = {
+    update.onActivation = true;
+    packages = [
+      { appId = "com.brave.Browser"; origin = "flathub";  }
+      "ch.openboard.OpenBoard" "org.vinegarhq.Sober"
+    ];
+  };
+  home = {
+    username = user;
+    homeDirectory = "/home/${user}";
+    stateVersion = "25.11";
+  };
+  programs.home-manager.enable = true;
