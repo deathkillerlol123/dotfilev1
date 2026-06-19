252aab5262ee49a0cd0fa73b2b06f1b5b00d320b
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-19 12:49:21 +0100

    Fri Jun 19 12:49:21 PM BST 2026

--- nixos/scripts/sys_sounds/sound_mon.sh
+++ nixos/scripts/sys_sounds/sound_mon.sh
@@ -1,3 +1,6 @@
 systemctl --user restart pipewire pipewire-pulse
-pactl set-default-sink alsa_output.pci-0000_00_1f.3.hdmi-stereo-extra1
-for i in $(pactl list short sink-inputs | awk '{print $1}'); do pactl move-sink-input $i alsa_output.pci-0000_00_1f.3.hdmi-stereo-extra1; done
+sink="alsa_output.pci-0000_00_1f.3.hdmi-stereo-extra1"
+pactl set-default-sink "$sink"
+pactl list short sink-inputs | awk '{print $1}' | while read -r i; do
+  pactl move-sink-input "$i" "$sink"
+done
