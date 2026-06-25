281142d2e1423be268a5a1c26adfeab35e6a1825
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-25 14:08:13 +0100

    Thu Jun 25 02:08:12 PM BST 2026

--- nixos/dots/nixboom/.config/waybar/random_wallpaper.sh
+++ nixos/dots/nixboom/.config/waybar/random_wallpaper.sh
@@ -5,24 +5,33 @@ cd ~/dotfiles/nixos/dots/nixboom/.config/waybar/
 COLORS="$HOME/.cache/wal"
 OUPUT="$HOME/dotfiles/nixos/dots/nixboom/.config"
 
-# Pick wallpaper
 FILE=$(find ~/Pictures/Wallpapers/ -type f -print0 | shuf -zn1 | xargs -0 echo)
 echo "$FILE"
 if [ -n "$FILE" ]; then
   swww img "$FILE" --transition-type fade --transition-step 90 --transition-duration 7 &
   wal -i "$FILE" -n
-
   cp "${COLORS}/colors-waybar.css" "${OUPUT}/waybar/colors.css"
- # cp "${COLORS}/colors.css" "${OUPUT}/swaync/"
-#  cp "${COLORS}/colors-kitty.conf" "${OUPUT}/kitty/"
-#  cp "${COLORS}/colors-zed.json" "${OUPUT}/zed/themes/pywal.json"
 
-  "$HOME/dotfiles/nixos/dots/nixboom/.config/waybar/css_rasi.sh"
+  INPUT="colors.css"
+  OUTPUT="colors.rasi"
+  
+  if [[ -z "$INPUT" || -z "$OUTPUT" ]]; then
+    echo "Usage: $0 input_file output_file"
+    exit 1
+  fi  
+  {
+    echo "* {"
+    grep '@define-color' "$INPUT" | while read -r line; do
+      name=$(echo "$line" | awk '{print $2}')
+      value=$(echo "$line" | awk '{print $3}' | tr -d ';')
+      echo "    $name: $value;"
+    done
+    echo "}"
+  } >"$OUTPUT"
+
   mv colors.rasi "${OUPUT}/rofi/colors.rasi"
   pkill -f rofi
-
   cd ~/dotfiles/nixos/bootloader/
   rm -rf *
   cp "$FILE" "$HOME/dotfiles/nixos/bootloader/"
-
 fi
