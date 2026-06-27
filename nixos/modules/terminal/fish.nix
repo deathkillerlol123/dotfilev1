ccb1fdd66b95f067645ad7c5e9c69ba2bf9070fe
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-27 18:00:13 +0100

    Sat Jun 27 06:00:13 PM BST 2026

--- nixos/modules/terminal/fish.nix
+++ nixos/modules/terminal/fish.nix
@@ -1,20 +1,22 @@
-{self,...}:{
-1;277;0c  flake.nixosModules.fish = {pkgs,...}:
-  {
-    programs.fish = {
-      enable = true;
-      shellAliases = {
-        gu="${self.outPath}/scripts/git_updater.sh";
-  	w="${self.outPath}/scripts/Boot-Windows10.sh";
-  	t="${self.outPath}/scripts/system_updaters/updatesys_Nixos.sh";
-  	n="gu && sudo nixos-rebuild switch --flake ~/dotfiles/nixos";		
-  	ls="eza -a --tree -L 1";
-  	e="emacs -nw";
-  	s="statix check ~/dotfiles/nixos";
+{ self, ... }:
+{
+  flake.nixosModules.fish =
+    { pkgs, ... }:
+    {
+      programs.fish = {
+        enable = true;
+        shellAliases = {
+          gu = "${self.outPath}/scripts/git_updater.sh";
+          w = "${self.outPath}/scripts/Boot-Windows10.sh";
+          t = "${self.outPath}/scripts/system_updaters/updatesys_Nixos.sh";
+          n = "gu && sudo nixos-rebuild switch --flake ~/dotfiles/nixos";
+          ls = "eza -a --tree -L 1";
+          e = "emacs -nw";
+          s = "statix check ~/dotfiles/nixos";
+        };
       };
+      environment.systemPackages = with pkgs; [
+        fish
+      ];
     };
-    environment.systemPackages = with pkgs; [
-      fish
-    ];
-  };
 }
