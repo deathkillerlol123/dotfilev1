644dbb5e2976bad1d9c2d38f65a64d3d2bc34def
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-29 23:28:24 +0100

    $(date)

--- nixos/modules/terminal/alias.nix
+++ nixos/modules/terminal/alias.nix
@@ -2,7 +2,7 @@
   flake.nixosModules.alias = {...}:
     {
       environment.shellAliases = {
-        gu = "${self.outPath}/scripts/git_updater.sh";
+        gu = "cd ~/dotfiles/ && git add . && git commit -m '$(date)' && git push -u orgin main ";
         w  = "${self.outPath}/scripts/Boot-Windows10.sh";
         n  = "sudo nixos-rebuild switch --flake ~/dotfiles/nixos --upgrade";
         t  = "gu && n && nix store gc && sudo nix store optimise && nh clean all --keep 3 && rm -rf ~/.cache/* && gu";
