9c35e9ebce75a4b425aff8119439e40bc70d9da5
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-29 22:29:42 +0100

    Mon Jun 29 10:29:42 PM BST 2026

--- nixos/modules/terminal/fish.nix
+++ nixos/modules/terminal/fish.nix
@@ -3,21 +3,12 @@
   flake.nixosModules.fish =
     { pkgs, ... }:
     {
+      imports = [alias];
       programs.fish = {
         enable = true;
-        shellAliases = {
-          gu = "${self.outPath}/scripts/git_updater.sh";
-          w = "${self.outPath}/scripts/Boot-Windows10.sh";
-          t = "${self.outPath}/scripts/system_updaters/updatesys_Nixos.sh";
-          n = "gu && sudo nixos-rebuild switch --flake ~/dotfiles/nixos --upgrade";
-          ls = "eza -a --tree -L 1";
-          e = "emacs -nw";
-          s = "statix check ~/dotfiles/nixos";
-          key = "wshowkeys -a bottom -m 20 -b 00000000";
+        shellAliases = self.lib.shellAliases;
         };
       };
-      environment.systemPackages = with pkgs; [
-        fish
-      ];
+      environment.systemPackages = [ pkgs.fish ];
     };
 }
