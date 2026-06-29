a135ae76f3e55957aeed3595183ae5c370e0df4b
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-29 18:58:03 +0100

    Mon Jun 29 06:58:03 PM BST 2026

--- nixos/modules/extras/gaming.nix
+++ nixos/modules/extras/gaming.nix
@@ -16,7 +16,7 @@
           gamescopeSession.enable = true;
           protontricks.enable = true;
           extest.enable = true;
-          platfornOptimizations.enable = true;          
+          platformOptimizations.enable = true;          
         };
       };
       hardware.graphics = {
