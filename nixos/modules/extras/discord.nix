f445ef43227f9eee5944b8a20e3dea8b611bc90c
deathkillerlol123 <khawajaahmedzeeshan123@gmail.com>
2026-06-30 14:50:31 +0100

    Tue Jun 30 02:50:31 PM BST 2026

--- nixos/modules/extras/discord.nix
+++ nixos/modules/extras/discord.nix
@@ -1,10 +1,25 @@
-{config,inputs,lib,pkgs,...}:{
-  flake.nixosModules.discord = {config,inputs,lib,pkgs,...}:{
-    imports = [ inputs.nixcord.homeModules.nixcord ];
-    programs.nixcord = {
-      enable = true;
-      discord =lib.mkForce {enable=false;};
-      vesktop.enable = true;
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  flake.nixosModules.discord =
+    {
+      config,
+      inputs,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      imports = [ inputs.nixcord.nixosModules.nixcord ];
+      programs.nixcord = {
+        enable = true;
+        discord = lib.mkForce { enable = false; };
+        vesktop.enable = true;
+      };
     };
-  };
 }
