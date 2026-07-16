67f61bd5db92e74498a97e582108c25fe37731a4
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-07-16 14:45:37 +0100

    Update

--- nixos/modules/hosts/users/nixboom/home.nix
+++ nixos/modules/hosts/users/nixboom/home.nix
@@ -45,7 +45,6 @@
       services.flatpak = {
         update.onActivation = true;
         packages = [
-          "ch.openboard.OpenBoard"
           "org.vinegarhq.Sober"
         ];
       };
@@ -80,13 +79,24 @@
           enable = true;
           discord = {
             vencord.enable = true;
+            krisp.enable = true;
           };
           config = {
             useQuickCss = true;
             autoUpdateNotification = true;
+            notifyAboutUpdates = true;
             themeLinks = [
               "https://raw.githubusercontent.com/refact0r/midnight-discord/0c6e4b5009df5f13fe33d9b279378378d5212330/themes/midnight.theme.css"
             ];
+            plugins = {
+              autoDndWhilePlaying.statusToSet = "dnd";
+              fakeNitro = {
+                enableStreamQualityBypass = true;
+              };
+              typingTweaks = {
+                showAvatars = true;
+              };
+            };
             frameless = true;
           };
         };
