e30c5469e9648a80e761c0d76e2e7a75d4e5f92c
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-07-20 21:09:09 +0100

    Update

--- nixos/modules/extras/gaming.nix
+++ nixos/modules/extras/gaming.nix
@@ -20,13 +20,18 @@
       hardware.graphics = {
         enable = true;
         enable32Bit = true;
+        extraPackages = with pkgs; [
+          intel-media-driver
+          vpl-gpu-rt
+        ];
+        extraPackages32 = with pkgs; [
+          driversi686Linux.intel-media-driver
+        ];
       };
       environment.systemPackages = with pkgs; [
         mangohud
         protonup-qt
         heroic
-        gamescope
-        gamemode
       ];
     };
 }
