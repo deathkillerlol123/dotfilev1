1380ae77e67307477b73ddc3676a7dfd16ea48e0
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-07-22 18:59:43 +0100

    Update

--- nixos/modules/hosts/users/nixboom/config.nix
+++ nixos/modules/hosts/users/nixboom/config.nix
@@ -1,25 +1,23 @@
-{self, ... }: {
-  flake.nixosModules.nixboom =  { lib,pkgs,inputs, ... }:
+{ self, ... }: {
+  flake.nixosModules.nixboom = {lib,pkgs,inputs,...}:
     let
       enabled = names: lib.genAttrs names (_: { enable = true; });
       imod = names: map (x: inputs.${x}.nixosModules.${x}) names;
       fmod = names: map (x: self.nixosModules.${x}) names;
     in
-      {
-        imports =
-          fmod [ "gaming" "lime" "main" "dragware" ] ++
-          imod [ "home-manager" "mango" ];
-        programs = enabled [ "wshowkeys" "mango" ];
-        lime = {
-          enable = true;
-          params = ["snd_hda_intel.dmic_detect=0" "snd_intel_dspcfg.dsp_driver=1" "8250.nr_uarts=1" ];
-        };
-        environment.systemPackages = with pkgs; [
-          wdisplays whatsapp-electron firefox
-          libreoffice awww  waypaper pywal swaylock rofi bzmenu pwmenu copyq
-          grim slurp swappy wl-clipboard ghostty python312
-          motrix
-        ];
-        system.stateVersion = "25.11";
+    {
+      imports = fmod [ "lime" "dragware" "gaming" "main" ] ++
+                imod [ "home-manager" "mango" ];
+      programs = enabled [ "wshowkeys" "mango" ];
+      lime = {
+        enable = true;
+        params = [ "snd_hda_intel.dmic_detect=0" "snd_intel_dspcfg.dsp_driver=1" "8250.nr_uarts=1" ];
       };
+      environment.systemPackages = with pkgs; [
+        ghostty wdisplays whatsapp-electron firefox #libreoffice
+        awww waypaper pywal swaylock rofi bzmenu pwmenu
+        copyq grim slurp swappy wl-clipboard
+      ];
+      system.stateVersion = "25.11";
+    };
 }
