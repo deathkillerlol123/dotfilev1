386a12732b7a7f09359636e89f9375fe21bec9df
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-07-24 23:50:35 +0100

    Update

--- nixos/modules/terminal/alias.nix
+++ nixos/modules/terminal/alias.nix
@@ -3,7 +3,7 @@
       environment.shellAliases = {
         gu = "$HOME/dotfiles/nixos/scripts/git_updater.sh";
         w  = "$HOME/dotfiles/nixos/scripts/Boot-Windows10.sh";
-        n  = "sudo nixos-rebuild switch --flake ~/dotfiles/nixos";
+        n  = "nh os switch $HOME/dotfiles/nixos/";
         t  = "gu && n && nix store gc && sudo nix store optimise && nh clean all --keep 3 && rm -rf ~/.cache/* && z ~/dotfiles/nixos && nix flake update && gu";
         ls = "eza -a --tree -L 1";
         e  = "emacs -nw";
