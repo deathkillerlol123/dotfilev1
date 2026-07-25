588b52dd3a0e5dc66421c9c6a226dba30267ca41
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-07-25 12:51:48 +0100

    Update

--- nixos/modules/extras/gaming.nix
+++ nixos/modules/extras/gaming.nix
@@ -4,7 +4,7 @@
       gamemode.enable = true;
       gamescope = {
         enable = true;
-        capSysNice = true;
+        capSysNice = false;
       };
       steam = {
         enable = true;
