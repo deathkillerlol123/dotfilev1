046c59e45dd3f1d40ed6d6d6e1eaad6378b635d1
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-07-26 19:07:48 +0100

    Update

--- nixos/modules/hosts/86-64-linux.nix
+++ nixos/modules/hosts/86-64-linux.nix
@@ -12,4 +12,25 @@ in {
       users
     ];
   };
+  flake.nixosModules.users = {...}: let
+    fmod = names: map (x: self.nixosModules.${x}) names;
+  in {
+    imports = fmod ["main-user" "home-man" "nixboom"];
+    main-user.users = {
+      nixboom = {
+        enable = true;
+        groups = ["wheel" "gamemode" "uinput"];
+      };
+      doct = {
+        enable = false;
+        groups = ["wheel" "gamemode"];
+      };
+    };
+    home-man = {
+      enable = true;
+      users = [
+        "nixboom"
+      ];
+    };
+  };
 }
