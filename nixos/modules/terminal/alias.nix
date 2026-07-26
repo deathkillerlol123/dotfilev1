f5cf364ac4a0925a3fb18656b8e5e521f57e071e
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-07-26 19:30:43 +0100

    Update

--- nixos/modules/terminal/alias.nix
+++ nixos/modules/terminal/alias.nix
@@ -4,7 +4,7 @@
         gu = "$HOME/dotfiles/nixos/scripts/git_updater.sh";
         w  = "$HOME/dotfiles/nixos/scripts/Boot-Windows10.sh";
         n  = "nh os switch $HOME/dotfiles/nixos/";
-        t  = "gu && nix flake update && n && nix store gc && sudo nix store optimise && nh clean all -k 2 && gu";
+        t  = "gu && z ~/dotfiles/nixos/ && nix flake update && n && nix store gc && sudo nix store optimise && nh clean all -k 2 && gu";
         ls = "eza -a --tree -L 1";
         e  = "emacs -nw";
         key = "wshowkeys -a bottom -m 20 -b 00000000";
