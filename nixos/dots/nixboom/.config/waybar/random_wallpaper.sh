6bb10dc6b33cdea2fa07ff010fb1bd60bd8b0b62
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-07-27 16:12:03 +0100

    Update

--- nixos/dots/nixboom/.config/waybar/random_wallpaper.sh
+++ nixos/dots/nixboom/.config/waybar/random_wallpaper.sh
@@ -1,32 +1,29 @@
 #!/usr/bin/env bash
-
-cd ~/dotfiles/nixos/dots/nixboom/.config/waybar/
+set -e
+exec 9>/tmp/random_wallpaper.lock
+flock -n 9 || exit 0
+pkill -x rofi -u "$USER" || true
+cd "$HOME/dotfiles/nixos/dots/nixboom/.config/waybar/"
 COLORS="$HOME/.cache/wal"
 FINALOUTPUT="$HOME/dotfiles/nixos/dots/nixboom/.config"
-FILE=$(find ~/Pictures/Wallpapers/ -type f -print0 | shuf -zn1 | xargs -0 echo)
+FILE=$(find "$HOME/Pictures/Wallpapers/" -type f -print0 | shuf -zn1 | xargs -0 echo)
 echo "$FILE"
 if [ -n "$FILE" ]; then
-  wal -i "$FILE" &&
-  cp ~/.cache/wal/ghostty.conf ~/dotfiles/nixos/dots/nixboom/.config/ghostty/ghostty.conf
-  cp "${COLORS}/colors-waybar.css" "${FINALOUTPUT}/waybar/colors.css"
-  INPUT="colors.css"
-  output_rasi="colors.rasi"
-  if [[ -z "$INPUT" || -z "$output_rasi" ]]; then
-    echo "Usage: $0 input_file output_file"
-    exit 1
-  fi  
-  {
-    echo "* {"
-    grep '@define-color' "$INPUT" | while read -r line; do
-      name=$(echo "$line" | awk '{print $2}')
-      value=$(echo "$line" | awk '{print $3}' | tr -d ';')
-      echo "    $name: $value;"
-    done
-    echo "}"
-  } >"${output_rasi}"
-  mv "${FINALOUTPUT}/waybar/colors.rasi" "${FINALOUTPUT}/rofi/colors.rasi"
-  pkill -x rofi -u "$USER" || true
-  cd ~/dotfiles/nixos/bootloader/
-  rm -rf * &&
-  cp "$FILE" "$HOME/dotfiles/nixos/bootloader/"
+    wal -i "$FILE"
+    cp "$HOME/.cache/wal/ghostty.conf" "$HOME/dotfiles/nixos/dots/nixboom/.config/ghostty/ghostty.conf"
+    cp "${COLORS}/colors-waybar.css" "${FINALOUTPUT}/waybar/colors.css"
+    INPUT="${FINALOUTPUT}/waybar/colors.css"
+    OUTPUT_RASI="${FINALOUTPUT}/rofi/colors.rasi"
+    {
+        echo "* {"
+        grep '@define-color' "$INPUT" | while read -r line; do
+            name=$(echo "$line" | awk '{print $2}')
+            value=$(echo "$line" | awk '{print $3}' | tr -d ';')
+            echo "    $name: $value;"
+        done
+        echo "}"
+    } > "$OUTPUT_RASI"
+    cd "$HOME/dotfiles/nixos/bootloader/"
+    rm -rf ./*
+    cp "$FILE" "$HOME/dotfiles/nixos/bootloader/"
 fi
