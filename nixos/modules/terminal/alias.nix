a85c236149e61446ccb21e07d3fc6af00c999a45
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-07-30 14:23:37 +0100

    Update

--- nixos/modules/terminal/alias.nix
+++ nixos/modules/terminal/alias.nix
@@ -1,7 +1,7 @@
 {self,...}:{
   flake.nixosModules.alias = {
       environment.shellAliases = {
-        gu = "cd ~/dotfiles/ && jj describe -m 'Update' && jj bookmark set main -r @ && jj git push && jj new &&";
+        gu = "cd ~/dotfiles/ && jj describe -m 'Update' && jj bookmark set main -r @ && jj git push && jj new";
         w  = "$HOME/dotfiles/nixos/scripts/Boot-Windows10.sh";
         n  = "nh os switch $HOME/dotfiles/nixos/";
         t  = "gu && z ~/dotfiles/nixos/ && nix flake update && n && nix store gc && sudo nix store optimise && nh clean all -k 2 && gu";
