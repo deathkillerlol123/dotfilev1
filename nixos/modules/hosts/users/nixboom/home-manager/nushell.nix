02de7dba015a4342aa8e89ed06372592dc629417
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-08-05 23:27:53 +0100

    Update

--- nixos/modules/hosts/users/nixboom/home-manager/nushell.nix
+++ nixos/modules/hosts/users/nixboom/home-manager/nushell.nix
@@ -8,6 +8,14 @@
         w = "~/dotfiles/nixos/modules/default/terminal/scripts/Boot-Windows10.sh";
       };
       extraConfig = ''
+        def gu [] {
+          cd ~/dotfiles/nixos
+          alejandra .
+          jj describe -m "Update"
+          jj bookmark set main -r @
+          jj git push
+          jj new
+        }
         def n [] {
           nh os switch ~/dotfiles/nixos/
         }
@@ -15,6 +23,22 @@
           nix store gc
           sudo nix store optimise
           nh clean all -k 2
+        }
+        def t [] {
+          cd ~/dotfiles/nixos
+          nix flake update
+          n
+          clean
+          gu
+        }
+        def key [] {
+          wshowkeys -a bottom -F "Sans Bold 30" -s "#B5B520ff" -f "#ecd29cff" -b "#201B1488" -t 1
+        }
+        def tv [] {
+          nix-search-tv print
+          | fzf --preview "nix-search-tv preview {}" --scheme history
+          | sed "s|^[^/]*/ *||"
+          | wl-copy
         }
       '';
     };
