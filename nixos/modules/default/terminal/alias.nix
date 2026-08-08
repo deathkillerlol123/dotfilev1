2a96a218b90acf2c86d48153fac7dcde38d84a9e
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-08-08 13:35:48 +0100

    Update

--- nixos/modules/default/terminal/alias.nix
+++ nixos/modules/default/terminal/alias.nix
@@ -5,7 +5,7 @@
       w = "$HOME/dotfiles/nixos/modules/default/terminal/scripts/Boot-Windows10.sh";
       n = "nh os switch $HOME/dotfiles/nixos/";
       clean = "nix store gc && sudo nix store optimise && nh clean all -k 2";
-      t = "cd ~/dotfiles/nixos && nix flake update  && n && clean && gu";
+      t = "cd ~/dotfiles/nixos && nix flake update && n && clean && gu";
       ls = "eza -a --tree -L 1";
       e = "emacs -nw";
       tv = "nix-search-tv print | fzf --preview 'nix-search-tv preview {}' --scheme history | sed 's|^[^/]*/ *||' | wl-copy";
