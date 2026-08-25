07f6fb9f9ff170fbf1f18c981f897ea24680130c
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-08-25 17:38:18 +0100

    Update

--- nixos/modules/hosts/86-64-linux.nix
+++ nixos/modules/hosts/86-64-linux.nix
@@ -8,7 +8,13 @@ in {
   flake.nixosConfigurations.${hostname} = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     specialArgs = {inherit inputs;};
-    modules = with self.nixosModules; [users];
+    modules = with self.nixosModules; [
+      main
+      users
+      {
+        system.stateVersion = "25.11";
+      }
+    ];
   };
   flake.nixosModules.users = {...}: {
     imports = [self.nixosModules.main-user] ++ (with self.nixosModules; [nixboom]);
--- nixos/modules/hosts/users/nixboom/config.nix
+++ nixos/modules/hosts/users/nixboom/config.nix
@@ -1,10 +1,9 @@
 {self, ...}: {
   flake.nixosModules.nixboom = {lib, ...}:
     {
-      imports = with self.nixosModules; [dragonfly main];
+      imports = with self.nixosModules; [dragonfly];
       programs.mango.enable = true;
       boot.kernelParams = ["snd_hda_intel.dmic_detect=0" "snd_intel_dspcfg.dsp_driver=1" "8250.nr_uarts=1"];
-      system.stateVersion = "25.11";
     }
     // lib.genAttrs ["gaming" "ble" "tone" "touch" "intel" "lime"] (_: {enable = true;});
 }
