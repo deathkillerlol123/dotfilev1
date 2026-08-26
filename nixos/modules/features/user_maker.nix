8f614133234be92916b279758c2898e6b2ecec7f
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-08-26 22:07:15 +0100

    Update

--- nixos/modules/features/user_maker.nix
+++ nixos/modules/features/user_maker.nix
@@ -10,43 +10,27 @@
     ...
   }: let
     cfg = config.main-user.users;
-
-    enabledUsers =
-      lib.filterAttrs (_: u: u.enable) cfg;
-
-    shells =
-      lib.unique (map (u: u.shell) (lib.attrValues enabledUsers));
+    enabledUsers = lib.filterAttrs (_: u: u.enable) cfg;
+    shells = lib.unique (map (u: u.shell) (lib.attrValues enabledUsers));
   in {
-    imports =
-      [inputs.home-manager.nixosModules.home-manager]
-      ++ lib.mapAttrsToList
-      (name: _: self.nixosModules.${name})
-      enabledUsers;
-
+    imports = [inputs.home-manager.nixosModules.home-manager];
     options.main-user.users = lib.mkOption {
-      type = lib.types.attrsOf (
-        lib.types.submodule (
-          {name, ...}: {
-            options = {
-              enable = lib.mkEnableOption "enable user";
-
-              shell = lib.mkOption {
-                type = lib.types.str;
-                default = "fish";
-                description = "shell type for the user";
-              };
-
-              groups = lib.mkOption {
-                type = lib.types.listOf lib.types.str;
-                default = ["wheel"];
-                description = "Groups that the user is in";
-              };
-            };
-          }
-        )
-      );
+      type = lib.types.attrsOf (lib.types.submodule ({name, ...}: {
+        options = {
+          enable = lib.mkEnableOption "enable user";
+          shell = lib.mkOption {
+            type = lib.types.str;
+            default = "fish";
+            description = "shell type for the user";
+          };
+          groups = lib.mkOption {
+            type = lib.types.listOf lib.types.str;
+            default = ["wheel"];
+            description = "Groups that the user is in";
+          };
+        };
+      }));
     };
-
     config = {
       users.users =
         lib.mapAttrs (name: u: {
@@ -55,21 +39,12 @@
           extraGroups = u.groups;
         })
         enabledUsers;
-
       home-manager = {
         useGlobalPkgs = true;
         useUserPackages = true;
-
-        extraSpecialArgs = {
-          inherit inputs self;
-        };
-
-        users =
-          lib.mapAttrs
-          (name: _: self.homeModules.${name})
-          enabledUsers;
+        extraSpecialArgs = {inherit inputs self;};
+        users = lib.mapAttrs (name: _: self.homeModules.${name}) enabledUsers;
       };
-
       security.sudo.extraRules =
         lib.mapAttrsToList (name: _: {
           users = [name];
@@ -81,7 +56,6 @@
           ];
         })
         enabledUsers;
-
       programs = lib.mkMerge [
         {
           nh = {
@@ -91,13 +65,9 @@
               extraArgs = "--keep 3";
             };
           };
-
           nix-ld.enable = true;
         }
-
-        (lib.genAttrs shells (_: {
-          enable = true;
-        }))
+        (lib.genAttrs shells (_: {enable = true;}))
       ];
     };
   };
--- nixos/modules/hosts/86-64-linux.nix
+++ nixos/modules/hosts/86-64-linux.nix
@@ -8,16 +8,10 @@ in {
   flake.nixosConfigurations.${hostname} = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     specialArgs = {inherit inputs;};
-    modules = with self.nixosModules; [
-      main
-      users
-      {
-        system.stateVersion = "25.11";
-      }
-    ];
+    modules = with self.nixosModules; [main users {system.stateVersion = "25.11";}];
   };
   flake.nixosModules.users = {...}: {
-    imports = [self.nixosModules.main-user];
+    imports = [self.nixosModules.main-user] ++ (with self.nixosModules; [nixboom]);
     main-user.users = {
       nixboom = {
         enable = true;
