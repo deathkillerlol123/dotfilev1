e5e09ce138467759109acc1db8a668773a78e1d6
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-09-03 00:02:56 +0100

    Update

--- nixos/modules/hosts/users/nixboom/home-manager/custom_scripts.nix
+++ nixos/modules/hosts/users/nixboom/home-manager/custom_scripts.nix
@@ -13,6 +13,40 @@
       (pkgs.writeShellScriptBin "clean_exit" ''
         loginctl terminate-session "$XDG_SESSION_ID"
       '')
+      (pkgs.writeShellScriptBin "backup" ''
+        set -euo pipefail
+        REPO="/mnt/Backups/nixos_backups"
+        BACKUP_NAME="$(date +%Y-%m-%d_%H-%M-%S)"
+        echo "Starting Borg backup: $BACKUP_NAME"
+        borg create \
+          --progress \
+          --stats \
+          --compression zstd \
+          "$REPO::$BACKUP_NAME" \
+          --exclude 'sh:**/Downloads/*.iso' \
+          --exclude 'sh:**/Downloads/*.tar.gz' \
+          --exclude 'sh:**/Downloads/tmp*' \
+          --exclude 'sh:**/Documents/tmp*' \
+          --exclude 'sh:**/Documents/codes/venv' \
+          --exclude 'sh:**/Downloads/isoss' \
+          "$HOME/Documents" \
+          "$HOME/Downloads" \
+          "$HOME/Pictures/Screenshots" \
+          "$HOME/dotfiles"
+        echo "Pruning old backups..."
+        borg prune \
+          --list \
+          --keep-daily=7 \
+          --keep-weekly=4 \
+          --keep-monthly=60 \
+          "$REPO"
+        echo "Updating backup list..."
+        echo borg list "$REPO"
+        echo "Unmounting backup drive..."
+        sync
+        sudo umount /mnt/Backups
+        echo "Backup complete."
+      '')
     ];
   };
 }
