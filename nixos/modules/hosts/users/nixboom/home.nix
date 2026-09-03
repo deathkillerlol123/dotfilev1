e55a0c1196531e5c9c3f4cadbbe53c3f445cc0df
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-09-03 11:58:44 +0100

    Update

--- nixos/modules/hosts/users/nixboom/home.nix
+++ nixos/modules/hosts/users/nixboom/home.nix
@@ -1,28 +1,7 @@
 {self, ...}: {
-  flake.homeModules.nixboom = {
-    config,
-    lib,
-    ...
-  }: let
+  flake.homeModules.nixboom = {config, ...}: let
     user = "nixboom";
     dotfiles = config.lib.file.mkOutOfStoreSymlink "/home/${user}/dotfiles/nixos/modules/hosts/users/${user}/home";
-    conf = "${dotfiles}/.config";
-    mkFile = names:
-      lib.genAttrs names (name: {
-        source = "${dotfiles}/${name}";
-      });
-    mkconf = names:
-      lib.listToAttrs (
-        map (name: {
-          name = ".config/${name}";
-          value = {
-            source = "${conf}/${name}";
-            recursive = true;
-            force = true;
-          };
-        })
-        names
-      );
   in {
     imports = with self.homeModules; [packages custom_scripts nbgtk nbgit nbdesktop nbmango nbfirefox nbdiscord nbstarship nbfish nbnu];
     programs.home-manager.enable = true;
@@ -30,9 +9,11 @@
       username = user;
       homeDirectory = "/home/${user}";
       stateVersion = "25.11";
-      file =
-        (mkconf ["rofi" "ghostty"])
-        // (mkFile [".emacs"]);
+      file = {
+        ".emacs".source = "${dotfiles}/.emacs";
+        ".config/rofi".source = "${dotfiles}/.config/rofi";
+        ".config/ghostty".source = "${dotfiles}/.config/ghostty";
+      };
     };
   };
 }
