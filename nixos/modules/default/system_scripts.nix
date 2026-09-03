aa9b6e7acb2dc787a931714bab84c7609123138f
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-09-03 19:40:13 +0100

    Update

--- nixos/modules/default/system_scripts.nix
+++ nixos/modules/default/system_scripts.nix
@@ -5,8 +5,11 @@
         set -euo pipefail
         win_boot=$(
           ${pkgs.efibootmgr}/bin/efibootmgr |
-            ${pkgs.gawk}/bin/awk -F'Boot|\*' \
-              '/Windows Boot Manager/ {print $2; exit}'
+            ${pkgs.gawk}/bin/awk '/Windows Boot Manager/ {
+              match($1, /^Boot([0-9A-Fa-f]+)/, m)
+              print m[1]
+              exit
+            }'
         )
         if [[ -n "$win_boot" ]]; then
           sudo ${pkgs.efibootmgr}/bin/efibootmgr -n "$win_boot"
