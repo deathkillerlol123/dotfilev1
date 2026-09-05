ecad25d23a28698bb4c5de8659d5bf21e546a104
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-09-05 19:07:08 +0100

    Update

--- nixos/modules/hosts/users/nixboom/config.nix
+++ nixos/modules/hosts/users/nixboom/config.nix
@@ -2,6 +2,5 @@
   flake.nixosModules.nixboom = {...}: {
     imports = with self.nixosModules; [gaming bluetooth sound touch intel lime];
     boot.kernelParams = ["snd_hda_intel.dmic_detect=0" "snd_intel_dspcfg.dsp_driver=1" "8250.nr_uarts=1"];
-    services.desktopManager.plasma6.enable = true;
   };
 }
