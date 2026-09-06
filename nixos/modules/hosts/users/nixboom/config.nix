8f97213c5f8c149e65e556493d4fca426800f00c
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-09-06 11:16:03 +0100

    Update

--- nixos/modules/hosts/users/nixboom/config.nix
+++ nixos/modules/hosts/users/nixboom/config.nix
@@ -1,6 +1,9 @@
 {self, ...}: {
-  flake.nixosModules.nixboom = {...}: {
+  flake.nixosModules.nixboom = {pkgs, ...}: {
     imports = with self.nixosModules; [gaming bluetooth sound touch intel lime];
     boot.kernelParams = ["snd_hda_intel.dmic_detect=0" "snd_intel_dspcfg.dsp_driver=1" "8250.nr_uarts=1"];
+    environment.systemPackages = with pkgs; [
+      polkit_gnome
+    ];
   };
 }
