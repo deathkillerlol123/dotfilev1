b074447057fb8dca6a2a5d26fdb9ba418838ccf2
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-09-11 17:46:53 +0100

    Update

--- nixos/modules/graphics/intel.nix
+++ nixos/modules/graphics/intel.nix
@@ -1,5 +1,8 @@
 {...}: {
   flake.nixosModules.intel = {pkgs, ...}: {
-    hardware.graphics.extraPackages = with pkgs; [intel-media-driver vpl-gpu-rt];
+    hardware.graphics = {
+      extraPackages = with pkgs; [intel-media-driver vpl-gpu-rt];
+      extraPackages32 = with pkgs; [driversi686Linux.intel-media-driver];
+    };
   };
 }
--- nixos/modules/hosts/users/nixboom/config.nix
+++ nixos/modules/hosts/users/nixboom/config.nix
@@ -2,5 +2,11 @@
   flake.nixosModules.nixboom = {...}: {
     imports = with self.nixosModules; [gaming bluetooth sound touch intel lime];
     boot.kernelParams = ["snd_hda_intel.dmic_detect=0" "snd_intel_dspcfg.dsp_driver=1" "8250.nr_uarts=1"];
+
+    environment.variables.VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/intel_icd.x86_64.json";
+    environment.variables.VK_ICD_FILENAMES_32 = "/run/opengl-driver-32/share/vulkan/icd.d/intel_icd.i686.json";
+    environment.variables.WLR_DRM_DEVICES = "/dev/dri/card0";
+    environment.variables.MESA_LOADER_DRIVER_OVERRIDE = "iris";
+    environment.variables.VK_DEVICE_SELECT = "Intel(R) Iris(R) Xe Graphics (TGL GT2)";
   };
 }
