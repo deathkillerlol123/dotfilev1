d20cce5178601a3ab94c26dd3f09047d4db4efbf
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2026-09-15 21:51:04 +0100

    Update

--- nixos/modules/hosts/users/nixboom/hm/desktop_files.nix
+++ nixos/modules/hosts/users/nixboom/hm/desktop_files.nix
@@ -1,5 +1,20 @@
 {...}: {
-  flake.homeModules.nbdesktop = {...}: {
+  flake.homeModules.nbdesktop = {pkgs, ...}: let
+    screen = pkgs.writeShellScriptBin "screen-menu" ''
+      output=$(wlr-randr | awk '/^[A-Za-z]+-[A-Za-z0-9-]+ "/ {print $1}' | rofi -dmenu -p "Monitor") || exit
+      [ -z "$output" ] && exit
+      action=$(printf 'On\nOff' | rofi -dmenu -p "$output") || exit
+      [ -z "$action" ] && exit
+      case "$action" in
+        Off) wlr-randr --output "$output" --off ;;
+        On)
+          pos=$(wlr-randr | awk -v o="$output" '$1==o{f=1} f&&/Position:/{print $2;exit}')
+          pos=$(printf '%s' "''${pos:-0,0}" | rofi -dmenu -p "Position")
+          [ -n "$pos" ] && wlr-randr --output "$output" --pos "$pos" --on
+          ;;
+      esac
+    '';
+  in {
     xdg.desktopEntries = {
       bluetooth = {
         name = "Bluetooth";
@@ -11,21 +26,7 @@
       };
       Screen = {
         name = "Screen";
-        exec = ''
-          sh -c '
-          output=$(wlr-randr | awk "/^[A-Za-z]+-[A-Za-z0-9-]+ \"/ {print \$1}" | rofi -dmenu -p "Monitor") || exit
-          [ -z "$output" ] && exit
-          action=$(printf "On\nOff" | rofi -dmenu -p "$output") || exit
-          [ -z "$action" ] && exit
-          case "$action" in
-            Off) wlr-randr --output "$output" --off ;;
-            On)
-              pos=$(wlr-randr | awk -v o="$output" "\$1==o{f=1} f&&/Position:/{print \$2;exit}")
-              pos=$(printf "%s" "''${pos:-0,0}" | rofi -dmenu -p "Position")
-              [ -n "$pos" ] && wlr-randr --output "$output" --pos "$pos" --on
-              ;;
-          esac
-        '';
+        exec = "${screen}/bin/screen-menu";
         terminal = false;
         type = "Application";
         icon = "utilities-terminal";
